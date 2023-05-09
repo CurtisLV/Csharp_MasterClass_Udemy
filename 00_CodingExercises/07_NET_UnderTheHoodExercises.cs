3ed489291dd4e7b63176db3155b4d9b8a9b8ad42
CurtisLV <curtislv@gmail.com>
2023-05-09 19:13:07 +0300

    09.05 5

--- 00_CodingExercises/07_NET_UnderTheHoodExercises.cs
+++ 00_CodingExercises/07_NET_UnderTheHoodExercises.cs
@@ -4,7 +4,7 @@ public static class RefModifierFastForwardToSummerExercise
     public static void FastForwardToSummer(ref DateTime date)
     {
         var firstDayOfSummer = new DateTime(date.Year, 6, 21);
-        if(date < firstDayOfSummer)
+        if (date < firstDayOfSummer)
         {
             date = firstDayOfSummer;
         }
