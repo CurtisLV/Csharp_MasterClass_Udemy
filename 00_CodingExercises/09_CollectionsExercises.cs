bc502f23d6bc7a61786a6516fa2f51d3f68bb906
CurtisLV <curtislv@gmail.com>
2023-05-09 19:13:25 +0300

    09.05 7

--- 00_CodingExercises/09_CollectionsExercises.cs
+++ 00_CodingExercises/09_CollectionsExercises.cs
@@ -4,8 +4,7 @@ public class PairOfArrays<TLeft, TRight>
     private readonly TLeft[] _left;
     private readonly TRight[] _right;
 
-    public PairOfArrays(
-        TLeft[] left, TRight[] right)
+    public PairOfArrays(TLeft[] left, TRight[] right)
     {
         _left = left;
         _right = right;
@@ -13,10 +12,7 @@ public class PairOfArrays<TLeft, TRight>
 
     public (TLeft Left, TRight Right) this[int indexInLeft, int indexInRight]
     {
-        get
-        {
-            return (_left[indexInLeft], _right[indexInRight]);
-        }
+        get { return (_left[indexInLeft], _right[indexInRight]); }
         set
         {
             _left[indexInLeft] = value.Left;
@@ -28,8 +24,7 @@ public class PairOfArrays<TLeft, TRight>
 //HashSet - CreateUnion method
 public static class HashSetsUnionExercise
 {
-    public static HashSet<T> CreateUnion<T>(
-        HashSet<T> set1, HashSet<T> set2)
+    public static HashSet<T> CreateUnion<T>(HashSet<T> set1, HashSet<T> set2)
     {
         var result = new HashSet<T>(set1);
 
@@ -45,8 +40,7 @@ public static class HashSetsUnionExercise
 //"params" keyword - Does Stack contain any of the given words?
 public static class StackExtensions
 {
-    public static bool DoesContainAny(
-        this Stack<string> stack, params string[] words)
+    public static bool DoesContainAny(this Stack<string> stack, params string[] words)
     {
         return words.Any(word => stack.Contains(word));
     }
@@ -55,8 +49,7 @@ public static class StackExtensions
 //yield statement - GetAllAfterLastNullReversed method
 public class YieldExercise
 {
-    public static IEnumerable<T> GetAllAfterLastNullReversed<T>(
-        IList<T> input)
+    public static IEnumerable<T> GetAllAfterLastNullReversed<T>(IList<T> input)
     {
         for (int i = input.Count - 1; i >= 0; i--)
         {
