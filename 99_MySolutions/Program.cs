ef9d39bd4e20f1b269424e552cc5345ae35e3178
CurtisLV <curtislv@gmail.com>
2023-05-10 18:21:23 +0300

    10.05 7

--- 99_MySolutions/Program.cs
+++ 99_MySolutions/Program.cs
@@ -4,7 +4,9 @@ Console.WriteLine("[A]dd a TODO");
 Console.WriteLine("[R]emove a TODO");
 Console.WriteLine("[E]xit");
 
+// Read line from console and assign to var
 string userChoice = Console.ReadLine();
 Console.WriteLine("User choice: " + userChoice);
 
+// TODO: handle user input
 Console.ReadKey();
