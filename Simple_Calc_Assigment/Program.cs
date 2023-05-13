9bc8574cb55948586ad00e9d40b0982d8a07f53a
CurtisLV <curtislv@gmail.com>
2023-05-13 13:45:43 +0300

    13.05. 6

---
+++ Simple_Calc_Assigment/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number: ");
+
+string userInputFirst = Console.ReadLine();
+int num1 = int.Parse(userInputFirst);
+
+Console.WriteLine("Input the second number: ");
