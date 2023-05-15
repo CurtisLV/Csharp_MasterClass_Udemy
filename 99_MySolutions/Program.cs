5db4d6427e7916357081d142a50a09e698b024b6
CurtisLV <curtislv@gmail.com>
2023-05-15 17:47:00 +0300

    15.05. 2

--- 99_MySolutions/Program.cs
+++ 99_MySolutions/Program.cs
@@ -1,54 +1,54 @@
-﻿Console.WriteLine("Hello!");
-Console.WriteLine("[S]ee all TODOs");
-Console.WriteLine("[A]dd a TODO");
-Console.WriteLine("[R]emove a TODO");
-Console.WriteLine("[E]xit");
+﻿//Console.WriteLine("Hello!");
+//Console.WriteLine("[S]ee all TODOs");
+//Console.WriteLine("[A]dd a TODO");
+//Console.WriteLine("[R]emove a TODO");
+//Console.WriteLine("[E]xit");
 
-// Read line from console and assign to var
-string userChoice = Console.ReadLine();
-Console.WriteLine("User choice: " + userChoice);
+//// Read line from console and assign to var
+//string userChoice = Console.ReadLine();
+//Console.WriteLine("User choice: " + userChoice);
 
-var result = Add(10, 5);
+//var result = Add(10, 5);
 
-bool isLong = IsLong(userChoice);
+//bool isLong = IsLong(userChoice);
 
-Console.WriteLine("Provide a number.");
+//Console.WriteLine("Provide a number.");
 
-string userInput = Console.ReadLine();
+//string userInput = Console.ReadLine();
 
-int num = int.Parse(userInput);
-Console.WriteLine(num);
+//int num = int.Parse(userInput);
+//Console.WriteLine(num);
 
-switch (userInput.ToUpper())
-{
-    case "S":
-        PrintSelectedOption("See all TODOs");
-        break;
-    case "A":
-        PrintSelectedOption("Add a TODO");
-        break;
-    case "R":
-        PrintSelectedOption("Remove a TODO");
-        break;
-    case "E":
-        PrintSelectedOption("Exit");
-        break;
-    default:
-        PrintSelectedOption("Invalid option");
-        break;
-}
+//switch (userInput.ToUpper())
+//{
+//    case "S":
+//        PrintSelectedOption("See all TODOs");
+//        break;
+//    case "A":
+//        PrintSelectedOption("Add a TODO");
+//        break;
+//    case "R":
+//        PrintSelectedOption("Remove a TODO");
+//        break;
+//    case "E":
+//        PrintSelectedOption("Exit");
+//        break;
+//    default:
+//        PrintSelectedOption("Invalid option");
+//        break;
+//}
 
-void PrintSelectedOption(string selectedOption)
-{
-    Console.WriteLine($"Selected option: {selectedOption}");
-}
+//void PrintSelectedOption(string selectedOption)
+//{
+//    Console.WriteLine($"Selected option: {selectedOption}");
+//}
 
-int Add(int a, int b)
-{
-    return a + b;
-}
+//int Add(int a, int b)
+//{
+//    return a + b;
+//}
 
-bool IsLong(string input)
-{
-    return input.Length > 10;
-}
+//bool IsLong(string input)
+//{
+//    return input.Length > 10;
+//}
