f12438c5a0b4f2d875c103e15aa438e92f3973ae
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-05-26 17:36:07 +0300

    26.05. 7

--- 03_CookiesCookbook/Recipes/Ingredients/Ingredient.cs
+++ 03_CookiesCookbook/Recipes/Ingredients/Ingredient.cs
@@ -4,9 +4,7 @@ public abstract class Ingredient
 {
     public abstract int Id { get; }
     public abstract string Name { get; }
-    public virtual string PreparationInstructions =>
-        "Add to other ingredients.";
+    public virtual string PreparationInstructions => "Add to other ingredients.";
 
-    public override string ToString() =>
-        $"{Id}. {Name}";
+    public override string ToString() => $"{Id}. {Name}";
 }
