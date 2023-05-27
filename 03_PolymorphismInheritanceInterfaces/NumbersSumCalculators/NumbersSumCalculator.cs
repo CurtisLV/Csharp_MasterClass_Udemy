9314b83d43631d012549912fbca621d434a337bd
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-05-27 21:05:08 +0300

    27.05. 4

--- 03_PolymorphismInheritanceInterfaces/NumbersSumCalculators/NumbersSumCalculator.cs
+++ 03_PolymorphismInheritanceInterfaces/NumbersSumCalculators/NumbersSumCalculator.cs
@@ -63,7 +63,7 @@
 //Console.WriteLine("As JSON:");
 //Console.WriteLine(asJson);
 
-//var personJson = 
+//var personJson =
 //    "{\"FirstName\":\"John\",\"LastName\":\"Smith\",\"YearOfBirth\":1972}";
 
 //var personFromJson = JsonSerializer.Deserialize<Person>(personJson);
@@ -76,4 +76,3 @@
 //    public string LastName { get; set; }
 //    public int YearOfBirth { get; set; }
 //}
-
