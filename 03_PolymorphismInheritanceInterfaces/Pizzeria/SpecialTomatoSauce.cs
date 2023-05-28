f64795c6ee743e5ea0283bd00d083544fb7dc667
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-05-28 22:11:32 +0300

    28.05 7

--- 03_PolymorphismInheritanceInterfaces/Pizzeria/SpecialTomatoSauce.cs
+++ 03_PolymorphismInheritanceInterfaces/Pizzeria/SpecialTomatoSauce.cs
@@ -2,12 +2,9 @@
 
 public class SpecialTomatoSauce : TomatoSauce
 {
-    public SpecialTomatoSauce(int priceIfExtraTopping) : base(priceIfExtraTopping)
-    {
-    }
+    public SpecialTomatoSauce(int priceIfExtraTopping)
+        : base(priceIfExtraTopping) { }
 
     //public override void Prepare() =>
     //    Console.WriteLine("Special tomato sauce");
 }
-
-
