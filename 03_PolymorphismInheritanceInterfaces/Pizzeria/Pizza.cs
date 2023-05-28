11636adcdc385a961bac48ff25813588484ab8c3
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-05-28 22:11:10 +0300

    28.05 5

--- 03_PolymorphismInheritanceInterfaces/Pizzeria/Pizza.cs
+++ 03_PolymorphismInheritanceInterfaces/Pizzeria/Pizza.cs
@@ -4,15 +4,10 @@ public class Pizza : IBakeable
 {
     private List<Ingredient> _ingredients = new List<Ingredient>();
 
-    public void AddIngredient(Ingredient ingredient) =>
-        _ingredients.Add(ingredient);
+    public void AddIngredient(Ingredient ingredient) => _ingredients.Add(ingredient);
 
     public string GetInstructions() =>
-        "Bake at 250 degrees Celsius for 10 minutes, " +
-        "ideally on a stone";
+        "Bake at 250 degrees Celsius for 10 minutes, " + "ideally on a stone";
 
-    public override string ToString() =>
-        $"This is a pizza with {string.Join(", ", _ingredients)}";
+    public override string ToString() => $"This is a pizza with {string.Join(", ", _ingredients)}";
 }
-
-
