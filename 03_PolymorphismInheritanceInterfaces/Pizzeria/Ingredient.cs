5a7dea4077f803101251a53a86b72e639c28bc71
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-05-28 22:10:41 +0300

    28.05 3

--- 03_PolymorphismInheritanceInterfaces/Pizzeria/Ingredient.cs
+++ 03_PolymorphismInheritanceInterfaces/Pizzeria/Ingredient.cs
@@ -4,8 +4,7 @@ public abstract class Ingredient
 {
     public Ingredient(int priceIfExtraTopping)
     {
-        Console.WriteLine(
-            "Constructor from the Ingredient class");
+        Console.WriteLine("Constructor from the Ingredient class");
         PriceIfExtraTopping = priceIfExtraTopping;
     }
 
@@ -19,14 +18,9 @@ public abstract class Ingredient
 
     public int PublicField;
 
-    public string PublicMethod() =>
-        "This method is PUBLIC in the Ingredient class.";
+    public string PublicMethod() => "This method is PUBLIC in the Ingredient class.";
 
-    protected string ProtectedMethod() =>
-        "This method is PROTECTED in the Ingredient class.";
+    protected string ProtectedMethod() => "This method is PROTECTED in the Ingredient class.";
 
-    private string PrivateMethod() =>
-        "This method is PRIVATE in the Ingredient class.";
+    private string PrivateMethod() => "This method is PRIVATE in the Ingredient class.";
 }
-
-
