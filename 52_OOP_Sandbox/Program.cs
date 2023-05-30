62d03b7804c5cdde7fe51472f2df359c0fa5b6af
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-05-30 17:08:29 +0300

    30.05. 6

--- 52_OOP_Sandbox/Program.cs
+++ 52_OOP_Sandbox/Program.cs
@@ -1,15 +1,15 @@
-﻿var internationalPizzaDay23 = new DateTime(2023, 2, 9);
+﻿//var internationalPizzaDay23 = new DateTime(2023, 2, 9);
 
-Console.WriteLine(internationalPizzaDay23.ToString());
-Console.WriteLine(internationalPizzaDay23.DayOfWeek);
+//Console.WriteLine(internationalPizzaDay23.ToString());
+//Console.WriteLine(internationalPizzaDay23.DayOfWeek);
 
-var internationalPizzaDay24 = internationalPizzaDay23.AddYears(1);
+//var internationalPizzaDay24 = internationalPizzaDay23.AddYears(1);
 
-Console.WriteLine();
-Console.WriteLine($"year is {internationalPizzaDay24.Year}");
-Console.WriteLine($"month is {internationalPizzaDay24.Month}");
-Console.WriteLine($"day is {internationalPizzaDay24.Day}");
-Console.WriteLine($"day of the week is {internationalPizzaDay24.DayOfWeek}");
-Console.WriteLine($"day of the year is {internationalPizzaDay24.DayOfYear}");
+//Console.WriteLine();
+//Console.WriteLine($"year is {internationalPizzaDay24.Year}");
+//Console.WriteLine($"month is {internationalPizzaDay24.Month}");
+//Console.WriteLine($"day is {internationalPizzaDay24.Day}");
+//Console.WriteLine($"day of the week is {internationalPizzaDay24.DayOfWeek}");
+//Console.WriteLine($"day of the year is {internationalPizzaDay24.DayOfYear}");
 
-Console.ReadKey();
+//Console.ReadKey();
