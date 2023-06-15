830d39c5655041460e9382ea3beaecb256868c99
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-06-15 14:39:46 +0300

    15.06. 12

--- 02_NamesBeforeRefactorToSRP/Program.cs
+++ 02_NamesBeforeRefactorToSRP/Program.cs
@@ -34,17 +34,17 @@ class NamesValidator
     }
 }
 
-class NamesRepository
+class StringsTextualRepository
 {
     private static readonly string Separator = Environment.NewLine;
 
-    public List<string> ReadFromTextFile(string filePath)
+    public List<string> Read(string filePath)
     {
         var fileContents = File.ReadAllText(filePath);
         return fileContents.Split(Separator).ToList();
     }
 
-    public void WriteToTextFile(string filePath, List<string> strings)
+    public void Write(string filePath, List<string> strings)
     {
         File.WriteAllText(filePath, string.Join(Separator, strings));
     }
