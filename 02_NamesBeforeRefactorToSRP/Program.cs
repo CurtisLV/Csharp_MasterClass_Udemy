044973f2f9a5084589b47ec686406efdd62652e2
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-06-18 11:03:35 +0300

    18.06. 7

--- 02_NamesBeforeRefactorToSRP/Program.cs
+++ 02_NamesBeforeRefactorToSRP/Program.cs
@@ -1,28 +1,38 @@
 ﻿using _2_NamesBeforeRefactorToSRP.DataAccess;
+using System.Diagnostics;
 
-var names = new Names();
-var path = new NamesFilePath().BuildFilePath();
-var stringsTextualRepository = new StringsTextualRepository();
-
-if (File.Exists(path))
+var stopwatch = Stopwatch.StartNew();
+for (int i = 0; i < 1000; i++)
 {
-    Console.WriteLine("Names file already exists. Loading names.");
-    var stringsFromFile = stringsTextualRepository.Read(path);
-    names.AddNames(stringsFromFile);
+    Console.WriteLine($"Loop number {i}");
 }
-else
-{
-    Console.WriteLine("Names file does not yet exist.");
 
-    //let's imagine they are given by the user
-    names.AddName("John");
-    names.AddName("not a valid name");
-    names.AddName("Claire");
-    names.AddName("123 definitely not a valid name");
+stopwatch.Stop();
+Console.WriteLine($"Time in ms: {stopwatch.ElapsedMilliseconds}");
 
-    Console.WriteLine("Saving names to the file.");
-    stringsTextualRepository.Write(path, names.All);
-}
-Console.WriteLine(new NamesFormatter().Format(names.All));
+//var names = new Names();
+//var path = new NamesFilePath().BuildFilePath();
+//var stringsTextualRepository = new StringsTextualRepository();
 
-Console.ReadLine();
+//if (File.Exists(path))
+//{
+//    Console.WriteLine("Names file already exists. Loading names.");
+//    var stringsFromFile = stringsTextualRepository.Read(path);
+//    names.AddNames(stringsFromFile);
+//}
+//else
+//{
+//    Console.WriteLine("Names file does not yet exist.");
+
+//    //let's imagine they are given by the user
+//    names.AddName("John");
+//    names.AddName("not a valid name");
+//    names.AddName("Claire");
+//    names.AddName("123 definitely not a valid name");
+
+//    Console.WriteLine("Saving names to the file.");
+//    stringsTextualRepository.Write(path, names.All);
+//}
+//Console.WriteLine(new NamesFormatter().Format(names.All));
+
+//Console.ReadLine();
