6648a1ae0c6f37fc2a22e9c7d021e74921b9deb1
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-06-19 18:01:01 +0300

    19.06. 2

--- 52_DiceRoll/Program.cs
+++ 52_DiceRoll/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var random = new Random();
+int number = random.Next(1, 7);
+
+Console.WriteLine("Dice rolled. Guess what number it shows in 3 tries.");
