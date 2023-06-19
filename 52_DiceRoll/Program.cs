0b7aaa8424eaa67e1a14658a93dbd4f3f17bb811
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-06-19 18:04:26 +0300

    19.06. 2

--- 52_DiceRoll/Program.cs
+++ 52_DiceRoll/Program.cs
@@ -1,4 +1,13 @@
 ﻿var random = new Random();
 int number = random.Next(1, 7);
+int maxTries = 3;
+int numberOfTries;
 
 Console.WriteLine("Dice rolled. Guess what number it shows in 3 tries.");
+
+do
+{
+    //
+} while (numberOfTries < maxTries);
+
+Console.ReadKey();
