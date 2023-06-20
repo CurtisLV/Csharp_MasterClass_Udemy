7255a319df29476e25ad1a2296d71beef43af44a
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-06-20 09:28:07 +0300

    20.06. 5

--- 52_DiceRoll/GameManager.cs
+++ 52_DiceRoll/GameManager.cs
@@ -1,4 +1,15 @@
 ﻿class GameManager
 {
-    //
+    private Random random;
+    private int numberToGuess;
+    private int maxTries;
+    private int numberOfTries;
+
+    public GameManager()
+    {
+        random = new Random();
+        numberToGuess = random.Next(1, 7);
+        maxTries = 3;
+        numberOfTries = 0;
+    }
 }
