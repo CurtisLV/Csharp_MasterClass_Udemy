3919778b89433fee5b61cb7aa967b335cf3bf50b
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-06-20 09:07:45 +0300

    20.06. 2

---
+++ 52_DiceRoll/InputValidator.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+class InputValidator
+{
+    bool InputValidation(string guess)
+    {
+        try
+        {
+            int.Parse(guess);
+        }
+        catch
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+Console.WriteLine($"The number was {numberToGuess}");
+Console.ReadKey();
--- 52_DiceRoll/Program.cs
+++ 52_DiceRoll/Program.cs
@@ -31,23 +31,3 @@ while (numberOfTries < maxTries)
 ;
 
 Console.WriteLine($"You lose!");
-class InputValidator
-{
-    bool InputValidation(string guess)
-    {
-        try
-        {
-            int.Parse(guess);
-        }
-        catch
-        {
-            return false;
-        }
-
-        return true;
-    }
-
-
-
-Console.WriteLine($"The number was {numberToGuess}");
-Console.ReadKey();
