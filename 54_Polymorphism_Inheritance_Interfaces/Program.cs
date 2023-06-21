48921f736af4d82f62bdb49fe65cd38dd71e6112
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-06-21 17:31:25 +0300

    21.06. 3

--- 54_Polymorphism_Inheritance_Interfaces/Program.cs
+++ 54_Polymorphism_Inheritance_Interfaces/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.ReadKey();
+
+public class Pizza
+{
+    private List<string> _ingredients = new List<string>();
+
+    public void AddIngridients(string ingredient) => _ingredients.Add(ingredient);
+
+    public string Describe() => $"This is a pizza with {string.Join(", ", _ingredients)}";
+}
