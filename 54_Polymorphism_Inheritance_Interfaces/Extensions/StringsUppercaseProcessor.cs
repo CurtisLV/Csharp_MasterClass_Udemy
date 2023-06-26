22139ca02c3d66447037d205d56b8cd6859e7efd
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-06-26 17:55:14 +0300

    26.06. 13

--- 54_Polymorphism_Inheritance_Interfaces/Extensions/StringsUppercaseProcessor.cs
+++ 54_Polymorphism_Inheritance_Interfaces/Extensions/StringsUppercaseProcessor.cs
@@ -4,11 +4,11 @@ public class StringsUppercaseProcessor : StringsProcessor
 {
     public override List<string> Process(List<string> result)
     {
-        //List<string> words = new List<string>();
-        //foreach (var word in result)
-        //{
-        //    words.Add(word.Substring(0, (word.Length / 2)));
-        //}
-        //return result;
+        List<string> words = new List<string>();
+        foreach (var word in result)
+        {
+            words.Add(word.ToUpper());
+        }
+        return words;
     }
 }
