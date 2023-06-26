4ac781bae7b55fe4ccce3dcdf9efde70492931e3
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-06-26 17:31:46 +0300

    26.06. 3

--- 54_Polymorphism_Inheritance_Interfaces/Extensions/StringProcessor.cs
+++ 54_Polymorphism_Inheritance_Interfaces/Extensions/StringProcessor.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace _54_Polymorphism_Inheritance_Interfaces.Extensions
 {
-    internal class StringProcessor
+    public class Exercise
     {
+        public List<string> ProcessAll(List<string> words)
+        {
+            var stringsProcessors = new List<StringsProcessor>
+            {
+                new StringsTrimmingProcessor(),
+                new StringsUppercaseProcessor()
+            };
+
+            List<string> result = words;
+            foreach (var stringsProcessor in stringsProcessors)
+            {
+                result = stringsProcessor.Process(result);
+            }
+            return result;
+        }
     }
 }
