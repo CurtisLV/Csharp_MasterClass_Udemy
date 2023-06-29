ece31239d01e44bbd583bb68dfb4a167d820bf4a
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-06-29 17:08:08 +0300

    29.06. 7

--- 54_Polymorphism_Inheritance_Interfaces/NumberSumCalc/NumberSumCalculator.cs
+++ 54_Polymorphism_Inheritance_Interfaces/NumberSumCalc/NumberSumCalculator.cs
@@ -1,21 +1,20 @@
-﻿class NumberSumCalculator
-{
-    var numbers = new List<int> { 1, 4, 6, -1, 12, 44, -8, -19 };
-    bool shallAddPositiveOnly = false;
+﻿//class NumberSumCalculator
+//{
+//    var numbers = new List<int> { 1, 4, 6, -1, 12, 44, -8, -19 };
+//    bool shallAddPositiveOnly = false;
 
-    NumbersSumCalculator calculator = shallAddPositiveOnly
-        ? new PositiveNumbersSumCalculator()
-        : new NumbersSumCalculator();
+//    NumbersSumCalculator calculator = shallAddPositiveOnly
+//        ? new PositiveNumbersSumCalculator()
+//        : new NumbersSumCalculator();
 
-    int sum;
-    if (shallAddPositiveOnly)
-    {
-        //sum = new PositiveNumbersSumCalculator().Calculate(numbers);
-    }
-    else
-    {
-        //sum = new NumbersSumCalculator().Calculate(numbers);
-   
-}   
-    }
+//    int sum;
+//    if (shallAddPositiveOnly)
+//    {
+//        //sum = new PositiveNumbersSumCalculator().Calculate(numbers);
+//    }
+//    else
+//    {
+//        //sum = new NumbersSumCalculator().Calculate(numbers);
 
+//}
+//    }
