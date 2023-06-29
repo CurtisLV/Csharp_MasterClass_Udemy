638263dfb0bcc861375d14fa154ebedc08bb0e11
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-06-29 17:07:12 +0300

    29.06. 6

---
+++ 54_Polymorphism_Inheritance_Interfaces/NumberSumCalc/NumbersSumCalculator.cs
@@ -0,0 +1,21 @@
+﻿class NumbersSumCalculator
+{
+    public int Calculate(List<int> numbers)
+    {
+        int sum = 0;
+        foreach (var num in numbers)
+        {
+            if (ShallBeAdded(num))
+            {
+                sum += num;
+            }
+        }
+        return sum;
+    }
+
+    protected virtual bool ShallBeAdded(int num)
+    {
+        return true;
+    }
+}
+
---
+++ 54_Polymorphism_Inheritance_Interfaces/NumberSumCalc/PositiveNumbersSumCalculator.cs
@@ -0,0 +1,8 @@
+﻿class PositiveNumbersSumCalculator : NumbersSumCalculator
+{
+    protected override bool ShallBeAdded(int num)
+    {
+        return num > 0;
+    }
+}
+
