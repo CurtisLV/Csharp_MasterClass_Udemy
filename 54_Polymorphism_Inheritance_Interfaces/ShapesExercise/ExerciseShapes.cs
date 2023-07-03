fa9b36c7da4369dfed3f0e4513ef8cac0bf9bc46
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-03 17:02:49 +0300

    03.07. 3

--- 54_Polymorphism_Inheritance_Interfaces/ShapesExercise/ExerciseShapes.cs
+++ 54_Polymorphism_Inheritance_Interfaces/ShapesExercise/ExerciseShapes.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace _54_Polymorphism_Inheritance_Interfaces.ShapesExercise
 {
-    internal class ExerciseShapes
+    public static class ExerciseShapes
     {
+        public static List<double> GetShapesAreas(List<Shape> shapes)
+        {
+            var result = new List<double>();
+
+            foreach (var shape in shapes)
+            {
+                result.Add(shape.CalculateArea());
+            }
+
+            return result;
+        }
+    }
+
+    //your code goes here - define the Shape class
+
+    public class Square : Shape
+    {
+        public double Side { get; }
+
+        public Square(double side)
+        {
+            Side = side;
+        }
+
+        //your code goes here
+    }
+
+    public class Rectangle : Shape
+    {
+        public double Width { get; }
+        public double Height { get; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        //your code goes here
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        //your code goes here
     }
 }
