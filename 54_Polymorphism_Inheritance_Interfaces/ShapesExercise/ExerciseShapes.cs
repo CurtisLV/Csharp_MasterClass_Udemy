f2548d0972d19302219e8a1e79cd60f230adbb55
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-03 17:25:54 +0300

    03.07. 11

--- 54_Polymorphism_Inheritance_Interfaces/ShapesExercise/ExerciseShapes.cs
+++ 54_Polymorphism_Inheritance_Interfaces/ShapesExercise/ExerciseShapes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _54_Polymorphism_Inheritance_Interfaces.ShapesExercise;
+﻿namespace _54_Polymorphism_Inheritance_Interfaces.ShapesExercise;
 
 public static class ExerciseShapes
 {
