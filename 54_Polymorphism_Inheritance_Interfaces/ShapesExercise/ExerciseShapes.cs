a3e5717c4696dd10ae2767cdce066901e15d7aa6
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-03 17:03:10 +0300

    03.07. 4

--- 54_Polymorphism_Inheritance_Interfaces/ShapesExercise/ExerciseShapes.cs
+++ 54_Polymorphism_Inheritance_Interfaces/ShapesExercise/ExerciseShapes.cs
@@ -4,60 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _54_Polymorphism_Inheritance_Interfaces.ShapesExercise
+namespace _54_Polymorphism_Inheritance_Interfaces.ShapesExercise;
+
+public static class ExerciseShapes
 {
-    public static class ExerciseShapes
+    public static List<double> GetShapesAreas(List<Shape> shapes)
     {
-        public static List<double> GetShapesAreas(List<Shape> shapes)
+        var result = new List<double>();
+
+        foreach (var shape in shapes)
         {
-            var result = new List<double>();
-
-            foreach (var shape in shapes)
-            {
-                result.Add(shape.CalculateArea());
-            }
-
-            return result;
-        }
-    }
-
-    //your code goes here - define the Shape class
-
-    public class Square : Shape
-    {
-        public double Side { get; }
-
-        public Square(double side)
-        {
-            Side = side;
+            result.Add(shape.CalculateArea());
         }
 
-        //your code goes here
+        return result;
     }
+}
 
-    public class Rectangle : Shape
+//your code goes here - define the Shape class
+
+public class Square : Shape
+{
+    public double Side { get; }
+
+    public Square(double side)
     {
-        public double Width { get; }
-        public double Height { get; }
-
-        public Rectangle(double width, double height)
-        {
-            Width = width;
-            Height = height;
-        }
-
-        //your code goes here
+        Side = side;
     }
 
-    public class Circle : Shape
+    //your code goes here
+}
+
+public class Rectangle : Shape
+{
+    public double Width { get; }
+    public double Height { get; }
+
+    public Rectangle(double width, double height)
     {
-        public double Radius { get; }
-
-        public Circle(double radius)
-        {
-            Radius = radius;
-        }
-
-        //your code goes here
+        Width = width;
+        Height = height;
     }
+
+    //your code goes here
+}
+
+public class Circle : Shape
+{
+    public double Radius { get; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    //your code goes here
 }
