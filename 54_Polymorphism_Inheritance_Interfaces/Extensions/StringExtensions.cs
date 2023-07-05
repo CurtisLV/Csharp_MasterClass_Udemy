18e5d07f53b0df3fdf7926bc95f7af51cb2cae24
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-05 16:52:36 +0300

    05.07. 7

--- 54_Polymorphism_Inheritance_Interfaces/Extensions/StringExtensions.cs
+++ 54_Polymorphism_Inheritance_Interfaces/Extensions/StringExtensions.cs
@@ -2,7 +2,5 @@
 
 public static class StringExtensions
 {
-    public static int CountLines {
-        //
-    }
+    public static int CountLines(this string input) => input.Split(Environment.NewLine).Length;
 }
