b84cf628bc5be9cf4bfd75fb74748c2f2b706625
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-05 16:58:13 +0300

    05.07. 11

--- 54_Polymorphism_Inheritance_Interfaces/Extensions/SeasonExtensions.cs
+++ 54_Polymorphism_Inheritance_Interfaces/Extensions/SeasonExtensions.cs
@@ -2,5 +2,10 @@
 
 public static class SeasonExtensions
 {
-    //
+    public static Season Next(this SeasonExtensions season)
+    {
+        int seasonAsInt = (int)season;
+        int nextSeason = (seasonAsInt + 1) % 4;
+        return (Season)nextSeason;
+    }
 }
