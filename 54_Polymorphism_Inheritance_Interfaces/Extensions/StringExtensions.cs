ad05758bce2d1045d02317de1e36f5706dfca22e
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-05 16:50:54 +0300

    05.07. 4

---
+++ 54_Polymorphism_Inheritance_Interfaces/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _54_Polymorphism_Inheritance_Interfaces.Extensions
+{
+    internal class StringExtensions { }
+}
