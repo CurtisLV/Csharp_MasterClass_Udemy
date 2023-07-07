7efce2977f368654f3fe4435dac7f0b2d2042ac4
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-07 15:59:56 +0300

    07.07. 7

---
+++ 54_Polymorphism_Inheritance_Interfaces/Flyables/Bird.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _54_Polymorphism_Inheritance_Interfaces.Flyables
+{
+    internal class Bird
+    {
+    }
+}
