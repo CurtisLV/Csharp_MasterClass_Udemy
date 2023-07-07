3ffaed9b72da5b1a1d33ad92dec503bb1eb57c8f
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-07 16:01:33 +0300

    07.07. 9

--- 54_Polymorphism_Inheritance_Interfaces/Flyables/Bird.cs
+++ 54_Polymorphism_Inheritance_Interfaces/Flyables/Bird.cs
@@ -2,5 +2,12 @@
 
 public class Bird
 {
+    public void Tweet() => Console.WriteLine("Tweet, tweet!");
+
+    public void Fly() => Console.WriteLine("Flying using its wings.");
+}
+
+public class Kite
+{
     //
 }
