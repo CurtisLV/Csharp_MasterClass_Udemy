26da172729025990a1e0dac88c587221400e35c2
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-09 14:05:04 +0300

    09.07. 1

--- 54_Polymorphism_Inheritance_Interfaces/Program.cs
+++ 54_Polymorphism_Inheritance_Interfaces/Program.cs
@@ -268,3 +268,25 @@ public static class ListExtension
         return result;
     }
 }
+
+// Third coding exercise
+
+public static class Exercise
+{
+    public static int Transform(int number)
+    {
+        var transformations = new List<INumericTransformation>
+        {
+            new By1Incrementer(),
+            new By2Multiplier(),
+            new ToPowerOf2Raiser()
+        };
+
+        var result = number;
+        foreach (var transformation in transformations)
+        {
+            result = transformation.Transform(result);
+        }
+        return result;
+    }
+}
