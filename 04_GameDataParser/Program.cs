6f2449a8c4d65ffc7dca68ad150e585e2789d874
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-11 20:02:21 +0300

    11.07. 2

--- 04_GameDataParser/Program.cs
+++ 04_GameDataParser/Program.cs
@@ -8,7 +8,8 @@ var app = new GameDataParserApp(
     userInteractor,
     new GamesPrinter(userInteractor),
     new VideoGamesDeserializer(userInteractor),
-    new LocalFileReader());
+    new LocalFileReader()
+);
 
 var logger = new Logger("log.txt");
 
@@ -19,8 +20,9 @@ try
 catch (Exception ex)
 {
     Console.WriteLine(
-        "Sorry! The application has experienced an unexpected error " +
-        "and will have to be closed.");
+        "Sorry! The application has experienced an unexpected error "
+            + "and will have to be closed."
+    );
     logger.Log(ex);
 }
 
