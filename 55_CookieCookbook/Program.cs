268639ab5648ee20ba6cbb606dd76b0befdfb0fd
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-11 19:49:45 +0300

    11.07. 1

--- 55_CookieCookbook/Program.cs
+++ 55_CookieCookbook/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿// define if saved in .txt or .json
+
+// if the defined file is not empty, print all existing recipes
+
+
+Console.WriteLine("Create a new cookie recipe! Available ingredients are:");
