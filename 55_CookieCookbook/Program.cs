9ce84f22c20bde2d4ba07ce25ff0a228ea98103d
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-19 16:36:08 +0300

    19.07. 3

--- 55_CookieCookbook/Program.cs
+++ 55_CookieCookbook/Program.cs
@@ -6,7 +6,66 @@
 Console.WriteLine("Create a new cookie recipe! Available ingredients are:");
 
 // Printing available ingredients
+// Initialize the list of ingredients
+List<Ingredient> ingredients = new List<Ingredient>()
+{
+    new Ingredient()
+    {
+        Id = 1,
+        Name = "Olive Oil",
+        Description = "Sauté or dress salads with it."
+    },
+    new Ingredient()
+    {
+        Id = 2,
+        Name = "Garlic",
+        Description = "Sauté for flavor or use in marinades."
+    },
+    new Ingredient()
+    {
+        Id = 3,
+        Name = "Onions",
+        Description = "Sauté for flavor or use as toppings."
+    },
+    new Ingredient()
+    {
+        Id = 4,
+        Name = "Salt",
+        Description = "Sprinkle for taste while cooking."
+    },
+    new Ingredient()
+    {
+        Id = 5,
+        Name = "Black Pepper",
+        Description = "Grind to enhance flavors in dishes."
+    },
+    new Ingredient()
+    {
+        Id = 6,
+        Name = "Lemon",
+        Description = "Squeeze juice or zest for flavor."
+    },
+    new Ingredient()
+    {
+        Id = 7,
+        Name = "Chicken Broth",
+        Description = "Simmer bones for flavorful liquid base."
+    },
+    new Ingredient()
+    {
+        Id = 8,
+        Name = "Soy Sauce",
+        Description = "Use as marinade or dipping sauce."
+    }
+};
 
+// Access and use the list of ingredients as needed
+foreach (Ingredient ingredient in ingredients)
+{
+    Console.WriteLine($"Ingredient: {ingredient.Name}");
+    Console.WriteLine($"Description: {ingredient.Description}");
+    Console.WriteLine();
+}
 
 public class Ingredient
 {
