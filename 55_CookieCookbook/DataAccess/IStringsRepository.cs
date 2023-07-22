fc470f2fd6cf9b7508c846199224257e8e417f96
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-22 14:26:21 +0300

    22.07. 7

---
+++ 55_CookieCookbook/DataAccess/IStringsRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace _55_CookieCookbook.DataAccess
+{
+    public interface IStringsRepository
+    {
+    }
+}
--- 55_CookieCookbook/DataAccess/StringsRepository.cs
+++ 55_CookieCookbook/DataAccess/StringsRepository.cs
@@ -2,6 +2,18 @@
 {
     public class StringsRepository : IStringsRepository
     {
-        //
+        public List<string> Read(string filePath)
+        {
+            //
+        }
+
+        public void Write(string filePath, List<string> strings)
+        {
+            //
+        }
+
+        // protected abstract List<string> TextToStrings(string fileContents);
+
+        // protected abstract string StringsToText(List<string> strings);
     }
 }
