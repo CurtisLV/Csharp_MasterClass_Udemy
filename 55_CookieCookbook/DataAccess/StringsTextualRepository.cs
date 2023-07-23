edb5bba12b7141969d8815d559c522c3d0691dd1
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-23 16:03:43 +0300

    23.07. 7

--- 55_CookieCookbook/DataAccess/StringsTextualRepository.cs
+++ 55_CookieCookbook/DataAccess/StringsTextualRepository.cs
@@ -4,13 +4,13 @@ class StringsTextualRepository : StringsRepository
 {
     private static readonly string Separator = Environment.NewLine;
 
-    protected override string StringsToText(List<string> strings)
-    {
-        throw new NotImplementedException();
-    }
-
     protected override List<string> TextToStrings(string fileContents)
     {
-        throw new NotImplementedException();
+        //
+    }
+
+    protected override string StringsToText(List<string> strings)
+    {
+        //
     }
 }
