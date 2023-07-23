eeab6b0634e5142855187766fdef0543dfb9a034
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-23 15:52:21 +0300

    23.07. 4

--- 55_CookieCookbook/DataAccess/StringsRepository.cs
+++ 55_CookieCookbook/DataAccess/StringsRepository.cs
@@ -9,13 +9,15 @@
             // else return empty List<string>
         }
 
+        // helps read file content
+        protected abstract List<string> TextToStrings(string fileContents);
+
         public void Write(string fileContents)
         {
             //
         }
 
-        protected abstract List<string> TextToStrings(string fileContents);
-
+        // helps write to file
         protected abstract string StringsToText(List<string> strings);
     }
 }
