6c74053c8ebbb82d4e4e694d1f54601c37865fa6
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-24 18:11:49 +0300

    24.07. 1

--- 55_CookieCookbook/DataAccess/StringsTextualRepository.cs
+++ 55_CookieCookbook/DataAccess/StringsTextualRepository.cs
@@ -4,13 +4,16 @@ class StringsTextualRepository : StringsRepository
 {
     private static readonly string Separator = Environment.NewLine;
 
+    // for Read
     protected override List<string> TextToStrings(string fileContents)
     {
-        //
+        // create a list of strings
+        // where each string is a line in text document (like 0, 1, 4)
     }
 
+    // for Write
     protected override string StringsToText(List<string> strings)
     {
-        //
+        // for each string, write it to a new line in text document
     }
 }
