8aebe5e9877423d6ef1fbbb1d99e842ee625922f
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-27 12:45:18 +0300

    27.07. 10

---
+++ 55_CookieCookbook/Recipes/Ingredients/SoySauce.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _55_CookieCookbook.Recipes.Ingredients
+{
+    public class SoySauce : Ingredient
+    {
+        public override int Id => 8;
+        public override string Name => "Soy Sauce";
+        public override string Description => "Use as marinade or dipping sauce.";
+    }
+}
