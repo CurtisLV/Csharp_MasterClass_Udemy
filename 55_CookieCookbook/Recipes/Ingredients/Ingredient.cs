67e1934bbba65998dd37049dcd699748d1a19316
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-27 12:20:00 +0300

    27.07. 2

---
+++ 55_CookieCookbook/Recipes/Ingredients/Ingredient.cs
@@ -0,0 +1,11 @@
+﻿public class Ingredient
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name}. {Description} Add to other ingredients.";
+    }
+}
