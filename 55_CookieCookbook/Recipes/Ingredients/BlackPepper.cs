89dee6a8f5b38e5ac0051f01a3e5e76867d07765
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-28 21:42:03 +0300

    28.07. 6

--- 55_CookieCookbook/Recipes/Ingredients/BlackPepper.cs
+++ 55_CookieCookbook/Recipes/Ingredients/BlackPepper.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace _55_CookieCookbook.Recipes.Ingredients;
 
-namespace _55_CookieCookbook.Recipes.Ingredients
+public class BlackPepper : Ingredient
 {
-    public class BlackPepper : Ingredient
-    {
-        public override int Id => 5;
-        public override string Name => "Black Pepper";
-        public override string Description => "Grind to enhance flavors in dishes.";
-    }
+    public override int Id => 5;
+    public override string Name => "Black Pepper";
+    public override string Description => "Grind to enhance flavors in dishes.";
 }
