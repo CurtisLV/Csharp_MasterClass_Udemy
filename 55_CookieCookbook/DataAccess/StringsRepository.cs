9b819cd0c335a307d5bd603b9975dde67a77896a
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-28 21:40:59 +0300

    28.07. 4

--- 55_CookieCookbook/DataAccess/StringsRepository.cs
+++ 55_CookieCookbook/DataAccess/StringsRepository.cs
@@ -1,28 +1,27 @@
-﻿namespace _55_CookieCookbook.DataAccess
+﻿namespace _55_CookieCookbook.DataAccess;
+
+public abstract class StringsRepository : IStringsRepository
 {
-    public abstract class StringsRepository : IStringsRepository
+    public List<string> Read(string filePath)
     {
-        public List<string> Read(string filePath)
+        // check if file exists
+        if (File.Exists(filePath))
         {
-            // check if file exists
-            if (File.Exists(filePath))
-            {
-                var fileContents = File.ReadAllText(filePath);
-                return TextToStrings(fileContents.Trim());
-            }
-            // else return empty List<string>
-            return new List<string>();
+            var fileContents = File.ReadAllText(filePath);
+            return TextToStrings(fileContents.Trim());
         }
-
-        // helps read file content
-        protected abstract List<string> TextToStrings(string fileContents);
-
-        public void Write(string filePath, List<string> strings)
-        {
-            File.AppendAllLines(filePath, strings);
-        }
-
-        // helps write to file
-        protected abstract string StringsToText(List<string> strings);
+        // else return empty List<string>
+        return new List<string>();
     }
+
+    // helps read file content
+    protected abstract List<string> TextToStrings(string fileContents);
+
+    public void Write(string filePath, List<string> strings)
+    {
+        File.AppendAllLines(filePath, strings);
+    }
+
+    // helps write to file
+    protected abstract string StringsToText(List<string> strings);
 }
