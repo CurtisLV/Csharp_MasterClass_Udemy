fbbc60baf883caa149e2456e2760c68b44ce0db5
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-28 21:39:44 +0300

    28.07. 3

---
+++ 55_CookieCookbook/Recipes/Ingredients/IIngredientRegister.cs
@@ -0,0 +1,9 @@
+﻿namespace _55_CookieCookbook.Recipes.Ingredients
+{
+    public interface IIngredientRegister
+    {
+        IEnumerable<Ingredient> All { get; }
+
+        Ingredient GetById(int id);
+    }
+}
