7fab565cd66562c433ffc71fe112266231ef8142
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-29 22:52:37 +0300

    29.07. 4

--- 55_CookieCookbook/Recipes/Ingredients/Lemon.cs
+++ 55_CookieCookbook/Recipes/Ingredients/Lemon.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace _55_CookieCookbook.Recipes.Ingredients;
 
-namespace _55_CookieCookbook.Recipes.Ingredients
+public class Lemon : Ingredient
 {
-    public class Lemon : Ingredient
-    {
-        public override int Id => 6;
-        public override string Name => "Lemon";
-        public override string Description => "Squeeze juice or zest for flavor.";
-    }
+    public override int Id => 6;
+    public override string Name => "Lemon";
+    public override string Description => "Squeeze juice or zest for flavor.";
 }
