37c2f24b3f1b368914e412dce8d2e338e5ffff32
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-29 22:53:21 +0300

    29.07. 7

--- 55_CookieCookbook/Recipes/Ingredients/Salt.cs
+++ 55_CookieCookbook/Recipes/Ingredients/Salt.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace _55_CookieCookbook.Recipes.Ingredients;
 
-namespace _55_CookieCookbook.Recipes.Ingredients
+public class Salt : Ingredient
 {
-    public class Salt : Ingredient
-    {
-        public override int Id => 4;
-        public override string Name => "Salt";
-        public override string Description => "Sprinkle for taste while cooking.";
-    }
+    public override int Id => 4;
+    public override string Name => "Salt";
+    public override string Description => "Sprinkle for taste while cooking.";
 }
