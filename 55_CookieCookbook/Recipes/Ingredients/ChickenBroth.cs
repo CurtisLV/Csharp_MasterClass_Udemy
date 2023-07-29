8cbd35c3fbb194ed9d6b86d800798b3d605beb98
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-29 22:51:43 +0300

    29.07. 1

--- 55_CookieCookbook/Recipes/Ingredients/ChickenBroth.cs
+++ 55_CookieCookbook/Recipes/Ingredients/ChickenBroth.cs
@@ -4,12 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _55_CookieCookbook.Recipes.Ingredients
+namespace _55_CookieCookbook.Recipes.Ingredients;
+
+public class ChickenBroth : Ingredient
 {
-    public class ChickenBroth : Ingredient
-    {
-        public override int Id => 7;
-        public override string Name => "Chicken Broth";
-        public override string Description => "Simmer bones for flavorful liquid base.";
-    }
+    public override int Id => 7;
+    public override string Name => "Chicken Broth";
+    public override string Description => "Simmer bones for flavorful liquid base.";
 }
