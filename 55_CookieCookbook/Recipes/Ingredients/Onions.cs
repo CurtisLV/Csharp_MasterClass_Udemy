9ad8bb76d2b5b8729ffeff931b6d492abd6988b0
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-29 22:53:06 +0300

    29.07. 6

--- 55_CookieCookbook/Recipes/Ingredients/Onions.cs
+++ 55_CookieCookbook/Recipes/Ingredients/Onions.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace _55_CookieCookbook.Recipes.Ingredients;
 
-namespace _55_CookieCookbook.Recipes.Ingredients
+public class Onions : Ingredient
 {
-    public class Onions : Ingredient
-    {
-        public override int Id => 3;
+    public override int Id => 3;
 
-        public override string Name => "Onions";
+    public override string Name => "Onions";
 
-        public override string Description => "Sauté for flavor or use as toppings.";
-    }
+    public override string Description => "Sauté for flavor or use as toppings.";
 }
