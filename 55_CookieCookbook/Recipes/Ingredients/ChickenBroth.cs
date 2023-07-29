8fc113d82e2c3c49f56182f92acefd3ccd796c57
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-29 22:51:53 +0300

    29.07. 2

--- 55_CookieCookbook/Recipes/Ingredients/ChickenBroth.cs
+++ 55_CookieCookbook/Recipes/Ingredients/ChickenBroth.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _55_CookieCookbook.Recipes.Ingredients;
+﻿namespace _55_CookieCookbook.Recipes.Ingredients;
 
 public class ChickenBroth : Ingredient
 {
