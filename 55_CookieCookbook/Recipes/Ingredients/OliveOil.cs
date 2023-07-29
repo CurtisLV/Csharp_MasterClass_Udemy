3721adb2bb1fdc50690e6872ad50fced4506f217
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-29 22:52:51 +0300

    29.07. 5

--- 55_CookieCookbook/Recipes/Ingredients/OliveOil.cs
+++ 55_CookieCookbook/Recipes/Ingredients/OliveOil.cs
@@ -1,12 +1,10 @@
-﻿namespace _55_CookieCookbook.Recipes.Ingredients
+﻿namespace _55_CookieCookbook.Recipes.Ingredients;
+
+public class OliveOil : Ingredient
 {
-    public class OliveOil : Ingredient
-    {
-        //
-        public override int Id => 1;
+    public override int Id => 1;
 
-        public override string Name => "Olive Oil";
+    public override string Name => "Olive Oil";
 
-        public override string Description => "Sauté or dress salads with it.";
-    }
+    public override string Description => "Sauté or dress salads with it.";
 }
