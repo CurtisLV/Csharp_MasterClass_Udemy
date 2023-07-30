4a5fbae3d5847f4b8a0aa2946821822ed8033cef
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-30 17:35:38 +0300

    30.07. 4

--- 07_NET_UnderTheHood_AssignmentStartupProject/Program.cs
+++ 07_NET_UnderTheHood_AssignmentStartupProject/Program.cs
@@ -10,19 +10,16 @@ var csvData = new CsvReader().Read(filePath);
 ITableDataBuilder tableDataBuiler = new TableDataBuilder();
 
 //JIT-ting the Test method
-var _ = TableDataPerformanceMeasurer.Test(
-    tableDataBuiler, csvData);
+var _ = TableDataPerformanceMeasurer.Test(tableDataBuiler, csvData);
 
-var testResult = TableDataPerformanceMeasurer.Test(
-    tableDataBuiler, csvData);
+var testResult = TableDataPerformanceMeasurer.Test(tableDataBuiler, csvData);
 
 Console.WriteLine("Test results for old code:");
-Console.WriteLine("Memory increase in bytes: " +
-    string.Format("{0:n0}", testResult.MemoryIncreaseInBytes));
-Console.WriteLine($"Time of loading the CSV was " +
-    $"{testResult.TimeOfBuildingTable}.");
-Console.WriteLine($"Time of reading the CSV was " +
-    $"{testResult.TimeOfDataReading}.");
+Console.WriteLine(
+    "Memory increase in bytes: " + string.Format("{0:n0}", testResult.MemoryIncreaseInBytes)
+);
+Console.WriteLine($"Time of loading the CSV was " + $"{testResult.TimeOfBuildingTable}.");
+Console.WriteLine($"Time of reading the CSV was " + $"{testResult.TimeOfDataReading}.");
 
 ITableDataBuilder fastTableDataBuiler = new FastTableDataBuilder();
 
@@ -42,8 +39,8 @@ ITableDataBuilder fastTableDataBuiler = new FastTableDataBuilder();
 //Console.WriteLine();
 //Console.WriteLine("Checking if results are the same...");
 //var areEqual = ContentEqualityChecker.IsEqual(
-//    tableDataBuiler, 
-//    fastTableDataBuiler, 
+//    tableDataBuiler,
+//    fastTableDataBuiler,
 //    csvData);
 
 //if(areEqual)
