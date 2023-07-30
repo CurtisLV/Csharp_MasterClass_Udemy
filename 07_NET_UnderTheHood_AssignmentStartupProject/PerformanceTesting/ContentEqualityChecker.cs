7be595740498653b4f16cd974f23d4a29abf39fb
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-07-30 17:35:12 +0300

    30.07. 1

--- 07_NET_UnderTheHood_AssignmentStartupProject/PerformanceTesting/ContentEqualityChecker.cs
+++ 07_NET_UnderTheHood_AssignmentStartupProject/PerformanceTesting/ContentEqualityChecker.cs
@@ -6,7 +6,8 @@ public class ContentEqualityChecker
     public static bool IsEqual(
         ITableDataBuilder leftTableDataBuiler,
         ITableDataBuilder rightTableDataBuiler,
-        CsvData csvData)
+        CsvData csvData
+    )
     {
         var leftData = leftTableDataBuiler.Build(csvData);
         var rightData = rightTableDataBuiler.Build(csvData);
@@ -29,10 +30,10 @@ public class ContentEqualityChecker
         {
             for (int i = 0; i < leftData.RowCount; ++i)
             {
-                if(!IsRowContentEqual(leftData, rightData, column, i))
+                if (!IsRowContentEqual(leftData, rightData, column, i))
                 {
                     return false;
-                }                
+                }
             }
         }
 
@@ -41,9 +42,10 @@ public class ContentEqualityChecker
 
     private static bool IsRowContentEqual(
         ITableData leftData,
-        ITableData rightData, 
-        string column, 
-        int rowIndex)
+        ITableData rightData,
+        string column,
+        int rowIndex
+    )
     {
         var leftValue = leftData.GetValue(column, rowIndex);
         var rightValue = rightData.GetValue(column, rowIndex);
@@ -53,8 +55,10 @@ public class ContentEqualityChecker
             return true;
         }
 
-        if ((leftValue is null && rightValue is not null) ||
-            (leftValue is not null && rightValue is null))
+        if (
+            (leftValue is null && rightValue is not null)
+            || (leftValue is not null && rightValue is null)
+        )
         {
             PrintRowContentIsNotEqual(column, rowIndex, leftValue, rightValue);
 
@@ -71,36 +75,36 @@ public class ContentEqualityChecker
         return true;
     }
 
-    private static void PrintColumnCountIsDifferent(
-        ITableData leftData,
-        ITableData rightData)
+    private static void PrintColumnCountIsDifferent(ITableData leftData, ITableData rightData)
     {
         Console.WriteLine(
-            $"Column count is not equal. " +
-            $"Left: {leftData.Columns.Count()}, " +
-            $"Right: {rightData.Columns.Count()}");
+            $"Column count is not equal. "
+                + $"Left: {leftData.Columns.Count()}, "
+                + $"Right: {rightData.Columns.Count()}"
+        );
     }
 
-    private static void PrintRowCountIsDifferent(
-        ITableData leftData,
-        ITableData rightData)
+    private static void PrintRowCountIsDifferent(ITableData leftData, ITableData rightData)
     {
         Console.WriteLine(
-            $"Row count is not equal. " +
-            $"Left: {leftData.RowCount}, " +
-            $"Right: {rightData.RowCount}");
+            $"Row count is not equal. "
+                + $"Left: {leftData.RowCount}, "
+                + $"Right: {rightData.RowCount}"
+        );
     }
 
     private static void PrintRowContentIsNotEqual(
-        string column, 
+        string column,
         int rowIndex,
         object leftValue,
-        object rightValue)
+        object rightValue
+    )
     {
         Console.WriteLine(
-            $"Row content is not equal " +
-            $"for column {column} and row number {rowIndex}. " +
-            $"Left: {leftValue}, " +
-            $"Right: {rightValue}");
+            $"Row content is not equal "
+                + $"for column {column} and row number {rowIndex}. "
+                + $"Left: {leftValue}, "
+                + $"Right: {rightValue}"
+        );
     }
 }
