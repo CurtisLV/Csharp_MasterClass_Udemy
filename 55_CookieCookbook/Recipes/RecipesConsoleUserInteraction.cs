bd31087031b82a7ede2266dd40b97160f32e747d
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-01 13:43:03 +0300

    01.08. 4

---
+++ 55_CookieCookbook/Recipes/RecipesConsoleUserInteraction.cs
@@ -0,0 +1,13 @@
+﻿public class RecipesConsoleUserInteraction
+{
+    public void ShowMessage(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+
+    public void Exit()
+    {
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
