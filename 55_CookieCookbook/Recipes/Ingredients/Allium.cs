7481366cb47ae5543f1cf4fd47e5fcb18d93459f
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-02 19:16:10 +0300

    02.08. 7

---
+++ 55_CookieCookbook/Recipes/Ingredients/Allium.cs
@@ -0,0 +1,7 @@
+﻿namespace _55_CookieCookbook.Recipes.Ingredients
+{
+    public abstract class Allium : Ingredient
+    {
+        public override string PreparationInstructions => "Sauté for flavor or use as marinades.";
+    }
+}
