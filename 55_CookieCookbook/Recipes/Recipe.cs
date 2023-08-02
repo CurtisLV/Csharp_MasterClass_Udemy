947d58d5490fb450e81e3e3b8734445488813a6e
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-02 19:06:31 +0300

    02.08. 4

--- 55_CookieCookbook/Recipes/Recipe.cs
+++ 55_CookieCookbook/Recipes/Recipe.cs
@@ -5,4 +5,9 @@ namespace _55_CookieCookbook.Recipes;
 public class Recipe
 {
     public List<Ingredient> Ingredients { get; }
+
+    public Recipe(List<Ingredient> ingredients)
+    {
+        Ingredients = ingredients;
+    }
 }
