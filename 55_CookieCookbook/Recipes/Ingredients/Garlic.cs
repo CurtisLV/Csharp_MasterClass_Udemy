8d21545f213904efe42a70b96a691ac79051b589
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-02 19:16:36 +0300

    02.08. 8

--- 55_CookieCookbook/Recipes/Ingredients/Garlic.cs
+++ 55_CookieCookbook/Recipes/Ingredients/Garlic.cs
@@ -1,10 +1,8 @@
 ﻿namespace _55_CookieCookbook.Recipes.Ingredients;
 
-public class Garlic : Ingredient
+public class Garlic : Allium
 {
     public override int Id => 2;
 
     public override string Name => "Garlic";
-
-    public override string PreparationInstructions => "Sauté for flavor or use in marinades.";
 }
