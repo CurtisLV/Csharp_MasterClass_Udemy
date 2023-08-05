c3ecc42c7c10f9a2637ae7119c89596af27b2446
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-05 14:02:15 +0300

    05.08 2

--- 55_CookieCookbook/Recipes/RecipesRepository.cs
+++ 55_CookieCookbook/Recipes/RecipesRepository.cs
@@ -4,6 +4,7 @@ using _55_CookieCookbook.Recipes;
 public class RecipesRepository : IRecipesRepository
 {
     StringsTextualRepository textRepository = new StringsTextualRepository();
+    private readonly IStringsRepository _stringsRepository;
 
     public List<string> Read(string filePath)
     {
@@ -12,6 +13,6 @@ public class RecipesRepository : IRecipesRepository
 
     internal void Write(string filePath, List<string> allRecipes)
     {
-        //
+        textRepository.Write(filePath, allRecipes);
     }
 }
