32900048952a903cc1a50e54dab7e250a789bdf9
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-05 14:14:31 +0300

    05.08 5

--- 55_CookieCookbook/Recipes/Ingredients/IngredientRegister.cs
+++ 55_CookieCookbook/Recipes/Ingredients/IngredientRegister.cs
@@ -17,14 +17,16 @@ public class IngredientRegister : IIngredientRegister
 
     public Ingredient GetById(int id)
     {
-        foreach (var ingredient in All)
-        {
-            if (ingredient.Id == id)
-            {
-                return ingredient;
-            }
-        }
+        //    foreach (var ingredient in All)
+        //    {
+        //        if (ingredient.Id == id)
+        //        {
+        //            return ingredient;
+        //        }
+        //    }
 
-        return null;
+        //    return null;
+        //}
+        return All.ToList().Find(ingredient => ingredient.Id == id);
     }
 }
