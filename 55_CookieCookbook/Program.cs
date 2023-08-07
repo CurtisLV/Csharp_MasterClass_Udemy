c8aaa8531d9dfaf1f88f6edcfd5a5e86e2ca8c62
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-07 13:36:24 +0300

    07.08 2

--- 55_CookieCookbook/Program.cs
+++ 55_CookieCookbook/Program.cs
@@ -29,27 +29,6 @@ var cookieRecipeApp = new CookieRecipeApp(
 );
 cookieRecipeApp.Run(fullFilePath);
 
-public class FileMetadata
-{
-    public string Name { get; }
-
-    public FileFormat Format { get; }
-
-    public FileMetadata(string name, FileFormat format)
-    {
-        Name = name;
-        Format = format;
-    }
-
-    public string ToPath() => $"{Name}.{Format.AsFileExtension()}";
-}
-
-public static class FileFormatExtensions
-{
-    public static string AsFileExtension(this FileFormat fileFormat) =>
-        fileFormat == FileFormat.Json ? "json" : "txt";
-}
-
 public class CookieRecipeApp
 {
     private readonly IRecipesRepository _recipesRepository;
