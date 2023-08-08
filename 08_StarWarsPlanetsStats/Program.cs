03b8d6ed5e706fad0efa48cec371de120087b8e1
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-08 20:14:36 +0300

    08.08. 1

--- 08_StarWarsPlanetsStats/Program.cs
+++ 08_StarWarsPlanetsStats/Program.cs
@@ -6,23 +6,21 @@ using StarWarsPlanetsStats.UserInteraction;
 try
 {
     var consoleUserInteractor = new ConsoleUserInteractor();
-    var planetsStatsUserInteractor = 
-        new PlanetsStatsUserInteractor(
-                consoleUserInteractor);
+    var planetsStatsUserInteractor = new PlanetsStatsUserInteractor(consoleUserInteractor);
 
     await new StarWarsPlanetsStatsApp(
         new PlanetsFromApiReader(
             new ApiDataReader(),
             new MockStarWarsApiDataReader(),
-            consoleUserInteractor),
-        new PlanetsStatisticsAnalyzer(
-            planetsStatsUserInteractor),
-        planetsStatsUserInteractor).Run();
+            consoleUserInteractor
+        ),
+        new PlanetsStatisticsAnalyzer(planetsStatsUserInteractor),
+        planetsStatsUserInteractor
+    ).Run();
 }
 catch (Exception ex)
 {
-    Console.WriteLine("An error occurred. " +
-        "Exception message: " + ex.Message);
+    Console.WriteLine("An error occurred. " + "Exception message: " + ex.Message);
 }
 
 Console.WriteLine("Press any key to close.");
