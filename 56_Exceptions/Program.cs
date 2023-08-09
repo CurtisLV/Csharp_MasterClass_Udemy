f51a6b872121f9a5d0707deec5520951c8762178
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-09 18:15:10 +0300

    09.08 6

--- 56_Exceptions/Program.cs
+++ 56_Exceptions/Program.cs
@@ -4,12 +4,16 @@ string input = Console.ReadLine();
 try
 {
     int number = ParseStringToInt(input);
+    Console.WriteLine("String successfully parsed, result is " + number);
 }
 catch (Exception)
 {
     Console.WriteLine("An exception was thrown.");
 }
-
+finally
+{
+    Console.WriteLine("Finally block is being executed!");
+}
 Console.ReadKey();
 
 int ParseStringToInt(string input)
