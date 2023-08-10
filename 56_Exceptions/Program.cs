0a075499a0f8353c20f9b078a0a4a7edf0b62ffa
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-10 16:55:52 +0300

    10.08. 6

--- 56_Exceptions/Program.cs
+++ 56_Exceptions/Program.cs
@@ -1,30 +1,31 @@
-﻿//Console.WriteLine("Enter a number:");
-//string input = Console.ReadLine();
+﻿Console.WriteLine("Enter a number:");
+string input = Console.ReadLine();
 
-//try
-//{
-//    int number = ParseStringToInt(input);
-//    Console.WriteLine("String successfully parsed, result is " + number);
-//}
-//catch (Exception ex)
-//{
-//    Console.WriteLine("An exception was thrown. Exception message: " + ex.Message);
-//}
-//finally
-//{
-//    Console.WriteLine("Finally block is being executed!");
-//}
-
+try
+{
+    int number = ParseStringToInt(input);
+    Console.WriteLine("String successfully parsed, result is " + number);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An exception was thrown. Exception message: " + ex.Message);
+}
+finally
+{
+    Console.WriteLine("Finally block is being executed!");
+}
 
 int ParseStringToInt(string input)
 {
     return int.Parse(input);
 }
 
+Console.ReadKey();
+
+// First coding exercise
 int result = DivideNumbers(4, 0);
 Console.WriteLine(result);
 
-// First coding exercise
 static int DivideNumbers(int a, int b)
 {
     try
@@ -41,4 +42,3 @@ static int DivideNumbers(int a, int b)
         Console.WriteLine("The DivideNumbers method ends.");
     }
 }
-Console.ReadKey();
