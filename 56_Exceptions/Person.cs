766294c02fec01ad3917ae91a5814c42e3314f72
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-10 17:34:48 +0300

    10.08. 12

---
+++ 56_Exceptions/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _56_Exceptions
+{
+    public class Person
+    {
+        public string Name { get; }
+
+        public int YearOfBirth { get; }
+
+        public Person(string name, int yearOfBirth)
+        {
+            Name = name;
+            YearOfBirth = yearOfBirth;
+        }
+    }
+}
