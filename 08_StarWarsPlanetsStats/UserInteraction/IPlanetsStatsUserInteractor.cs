b636cbde3a47443976ef25c3c917e1cee6264c4d
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-12 09:39:33 +0300

    12.08. 5

--- 08_StarWarsPlanetsStats/UserInteraction/IPlanetsStatsUserInteractor.cs
+++ 08_StarWarsPlanetsStats/UserInteraction/IPlanetsStatsUserInteractor.cs
@@ -5,7 +5,6 @@ namespace StarWarsPlanetsStats.UserInteraction;
 public interface IPlanetsStatsUserInteractor
 {
     void Show(IEnumerable<Planet> planets);
-    string? ChooseStaticticsToBeShown(
-        IEnumerable<string> propertiesThatCanBeChosen);
+    string? ChooseStaticticsToBeShown(IEnumerable<string> propertiesThatCanBeChosen);
     void ShowMessage(string message);
 }
