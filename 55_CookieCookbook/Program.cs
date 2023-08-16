3137708f215eaf4acc96ee24ba6083b727b0eb02
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-16 16:47:29 +0300

    16.08. 10

--- 55_CookieCookbook/Program.cs
+++ 55_CookieCookbook/Program.cs
@@ -6,35 +6,35 @@ using _55_CookieCookbook.Recipes.Ingredients;
 
 try
 {
+    // define if saved in .txt or .json
+    const FileFormat Format = FileFormat.Json;
 
+    //const FileFormat Format = FileFormat.Txt;
+
+    const string FileName = "recipes";
+    const string BaseDirectory =
+        "C:\\Users\\s3257b\\Desktop\\github.CurtisLV\\Csharp_MasterClass_Udemy\\55_CookieCookbook\\Files";
+
+    //string filePath = Format == FileFormat.Json ? $"{FileName}.json" : $"{FileName}.txt";
+
+    var fileMetadata = new FileMetadata(FileName, Format);
+
+    string fullFilePath = BaseDirectory + "\\" + fileMetadata.ToPath();
+
+    IStringsRepository stringsRepository =
+        Format == FileFormat.Json ? new StringsJsonRepository() : new StringsTextualRepository();
+
+    var ingredientsRegister = new IngredientRegister();
+
+    var cookieRecipeApp = new CookieRecipeApp(
+        new RecipesRepository(stringsRepository, ingredientsRegister),
+        new RecipesConsoleUserInteraction(ingredientsRegister)
+    );
+    cookieRecipeApp.Run(fullFilePath);
 }
 catch (Exception)
 {
 
 	throw;
 }
-// define if saved in .txt or .json
-const FileFormat Format = FileFormat.Json;
 
-//const FileFormat Format = FileFormat.Txt;
-
-const string FileName = "recipes";
-const string BaseDirectory =
-    "C:\\Users\\s3257b\\Desktop\\github.CurtisLV\\Csharp_MasterClass_Udemy\\55_CookieCookbook\\Files";
-
-//string filePath = Format == FileFormat.Json ? $"{FileName}.json" : $"{FileName}.txt";
-
-var fileMetadata = new FileMetadata(FileName, Format);
-
-string fullFilePath = BaseDirectory + "\\" + fileMetadata.ToPath();
-
-IStringsRepository stringsRepository =
-    Format == FileFormat.Json ? new StringsJsonRepository() : new StringsTextualRepository();
-
-var ingredientsRegister = new IngredientRegister();
-
-var cookieRecipeApp = new CookieRecipeApp(
-    new RecipesRepository(stringsRepository, ingredientsRegister),
-    new RecipesConsoleUserInteraction(ingredientsRegister)
-);
-cookieRecipeApp.Run(fullFilePath);
