110ea539e703945d4fade2592260b01d005a0dd4
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-16 16:49:43 +0300

    16.08. 12

--- 55_CookieCookbook/Program.cs
+++ 55_CookieCookbook/Program.cs
@@ -33,7 +33,8 @@ try
 }
 catch (Exception ex)
 {
-
+    Console.WriteLine($"Sorry! The application experienced an unexpected error and will have to be closed." 
+        + $"The error message: {ex.Message}");
 	throw;
 }
 
