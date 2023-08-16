6abe0b05880cfbe612119f7cbff7b0a6d8cf1ce8
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-16 16:27:52 +0300

    16.08. 4

--- 56_Exceptions/PersonDataReader.cs
+++ 56_Exceptions/PersonDataReader.cs
@@ -10,6 +10,19 @@ public class PersonDataReader
 {
     private readonly IPeopleRepository _peopleRepository;
     private readonly ILogger _logger;
+
+    public PersonDataReader(IPeopleRepository personRepository, ILogger logger)
+    {
+        _peopleRepository = personRepository;
+        _logger = logger;
+    }
+
+    public Person ReadPersonData(int personId)
+    {
+        return _peopleRepository.Read(personId);
+    }
+
+
 }
 
 public interface ILogger
