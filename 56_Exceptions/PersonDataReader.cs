14cdf2027f8e46fb3e7b9df4793d1a5adb002168
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-16 16:24:13 +0300

    16.08. 2

--- 56_Exceptions/PersonDataReader.cs
+++ 56_Exceptions/PersonDataReader.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _56_Exceptions
+namespace _56_Exceptions;
+
+public class PersonDataReader
 {
-    internal class PersonDataReader
-    {
-    }
+    //
 }
