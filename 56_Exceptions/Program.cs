49da003021bc1c65e02f6215f64544c269ce139e
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-17 16:52:56 +0300

    17.08. 6

--- 56_Exceptions/Program.cs
+++ 56_Exceptions/Program.cs
@@ -79,6 +79,11 @@ catch (HttpRequestException ex) when (ex.Message == "404")
     Console.WriteLine("The resource was not found.");
     throw;
 }
+catch (HttpRequestException ex) when (ex.Message.StartsWith("4"))
+{
+    Console.WriteLine("Some kind of client error.");
+    throw;
+}
 catch (HttpRequestException ex) when (ex.Message == "500")
 {
     Console.WriteLine("The server has experienced an internal error.");
