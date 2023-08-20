e659e2a305977d18fdb2f77693d969326b0bc9f3
Kārlis Mukāns <karlis.mukans@seb.se>
2023-08-20 10:48:04 +0300

    20.08. 7

--- 56_Exceptions/CustomExceptions/CustomException.cs
+++ 56_Exceptions/CustomExceptions/CustomException.cs
@@ -7,12 +7,12 @@ public class CustomException : Exception
         //
     }
 
-    public CustomException()
+    public CustomException(string message) : base(message)
     {
         //
     }
 
-    public CustomException()
+    public CustomException(string message, Exception innerException) : base(message, innerException)
     {
         //
     }
