727e2c413b95705fdd424d025402321ea27ea251
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-21 16:13:51 +0300

    21.08. 9

--- 56_Exceptions/Program.cs
+++ 56_Exceptions/Program.cs
@@ -199,11 +199,13 @@ public class InvalidTransactionException : Exception
     }
 
     public InvalidTransactionException(string message)
+        : base(message)
     {
         //
     }
 
     public InvalidTransactionException(string message, Exception innerException)
+        : base(message, innerException)
     {
         //
     }
