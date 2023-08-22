659fbe490a9aef12ddb14eec085be90d7e9ac132
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-22 18:06:21 +0300

    22.08. 4

--- 57_GameDataParser/Program.cs
+++ 57_GameDataParser/Program.cs
@@ -1,7 +1,10 @@
 ﻿Console.WriteLine("Enter the name of the file you want to read:");
 var input = Console.ReadLine();
 
-while (true)
+while (input != null || input != "")
 {
-    //
+    Console.WriteLine("Hello!");
 }
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
