f19593708011564003e9c768fc452b05b074c435
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-23 16:16:06 +0300

    23.08. 2

--- 57_GameDataParser/Program.cs
+++ 57_GameDataParser/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Text.RegularExpressions;
-
-var input = "";
+﻿var input = "";
 
 while (input == null || input == "")
 {
@@ -12,19 +10,20 @@ while (input == null || input == "")
     if (File.Exists(input))
     {
         // try to parse the whole JSON file
+        Console.WriteLine("File exists!");
     }
     else
     {
-        //
+        Console.WriteLine("File doesn't exist!");
     }
 }
 
 Console.WriteLine("Press any key to close.");
 Console.ReadKey();
 
-static bool IsJsonFile(string filename)
-{
-    // Using regex to match the "*.json" file format
-    string pattern = @"\.json$";
-    return Regex.IsMatch(filename, pattern, RegexOptions.IgnoreCase);
-}
+//static bool IsJsonFile(string filename)
+//{
+//    // Using regex to match the "*.json" file format
+//    string pattern = @"\.json$";
+//    return Regex.IsMatch(filename, pattern, RegexOptions.IgnoreCase);
+//}
