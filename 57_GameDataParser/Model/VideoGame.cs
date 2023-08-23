6c6e7eea0f83faac9ec2bf76ec9fb870dc7595ac
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-23 16:43:27 +0300

    23.08. 12

---
+++ 57_GameDataParser/Model/VideoGame.cs
@@ -0,0 +1,8 @@
+﻿namespace _57_GameDataParser;
+
+public class VideoGame
+{
+    public string Title { get; set; }
+    public int ReleaseYear { get; set; }
+    public double Rating { get; set; }
+}
