41fb1afd29908ab7897adb6750ee119e603d28ad
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-24 18:15:52 +0300

    24.08. 9

--- 57_GameDataParser/Logging/Logger.cs
+++ 57_GameDataParser/Logging/Logger.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace _57_GameDataParser.Logging;
 
-namespace _57_GameDataParser.Logging
+public class Logger
 {
-    internal class Logger { }
+    //
 }
