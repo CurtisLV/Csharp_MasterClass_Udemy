a9b04dea40c218e21443565797291d7e3673a4f9
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-25 20:27:28 +0300

    25.08. 7

--- 57_GameDataParser/Program.cs
+++ 57_GameDataParser/Program.cs
@@ -42,11 +42,12 @@ do
         }
         catch (JsonException ex)
         {
-            throw new JsonException($"{ex.Message} The file is: {fileName}", ex);
+            //throw new JsonException($"{ex.Message} The file is: {fileName}", ex);
+            var originalConsoleForegroundColor = Console.ForegroundColor;
             Console.WriteLine($"JSON in the {fileName} was not in a valid format. JSON body:");
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine(jsonContent);
-            Console.ResetColor();
+            Console.ForegroundColor = originalConsoleForegroundColor;
             Console.WriteLine(
                 "Sorry! The application has experienced an unexpected error and will have to be closed."
             );
