7c3d98bdfd7d9d4648c6ffbf388098e8120ff7d3
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-26 11:23:32 +0300

    26.08. 12

--- 57_GameDataParser/Program.cs
+++ 57_GameDataParser/Program.cs
@@ -3,7 +3,8 @@ using _57_GameDataParser.Model;
 using _57_GameDataParser.Logging;
 
 var app = new GameDataParserApp();
-
+string logFileName = "log.txt";
+var logger = Logger.CreateLogger(logFileName);
 try
 {
     app.Run();
@@ -13,6 +14,7 @@ catch (Exception ex)
     Console.WriteLine(
         "Sorry! The app has experienced an unexpected error and will have to be closed."
     );
+    logger.Information(ex.ToString());
 }
 
 Console.WriteLine("Press any key to close.");
