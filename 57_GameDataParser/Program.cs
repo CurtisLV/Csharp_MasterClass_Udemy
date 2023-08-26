7c20313c33b495e3d20184a9900ab0b923fd2d6a
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-26 11:08:53 +0300

    26.08. 5

--- 57_GameDataParser/Program.cs
+++ 57_GameDataParser/Program.cs
@@ -1,17 +1,3 @@
 ﻿using _57_GameDataParser.Model;
 
-static void PrintVideoGames(List<VideoGame> videoGames)
-{
-    if (videoGames.Count > 0)
-    {
-        Console.WriteLine("Loaded games are:");
-        foreach (VideoGame vg in videoGames)
-        {
-            Console.WriteLine(vg.ToString());
-        }
-    }
-    else
-    {
-        Console.WriteLine("No games are present in the input file.");
-    }
-}
+
