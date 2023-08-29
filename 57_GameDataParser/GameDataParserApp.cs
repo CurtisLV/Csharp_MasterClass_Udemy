815602cfd36b2062c61a60e0f56452cc8478f5ae
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-29 13:30:49 +0300

    29.08. 12

--- 57_GameDataParser/GameDataParserApp.cs
+++ 57_GameDataParser/GameDataParserApp.cs
@@ -10,25 +10,41 @@ public class GameDataParserApp
     private readonly IUserInteractor _userInteractor;
     private readonly IGamesPrinter _gamesPrinter;
     private readonly IVideoGamesDeserializer _videoGamesDeserializer;
+    private readonly IFileReader _fileReader;
 
     public GameDataParserApp(
         IUserInteractor userInteractor,
         IGamesPrinter gamesPrinter,
-        IVideoGamesDeserializer videoGamesDeserializer = null
+        IVideoGamesDeserializer videoGamesDeserializer,
+        IFileReader fileReader
     )
     {
         _userInteractor = userInteractor;
         _gamesPrinter = gamesPrinter;
         _videoGamesDeserializer = videoGamesDeserializer;
+        _fileReader = fileReader;
     }
 
     public void Run()
     {
         string fileName = _userInteractor.ReadValidFilePath();
-        var fileContents = File.ReadAllText(fileName);
+        var fileContents = _fileReader.Read(fileName);
         List<VideoGame> videoGames = _videoGamesDeserializer.Deserialize(fileName, fileContents);
         // print all games to console
         _gamesPrinter.Print(videoGames);
+    }
+}
+
+public interface IFileReader
+{
+    string Read(string fileName);
+}
+
+public class LocalFileReader : IFileReader
+{
+    public string Read(string fileName)
+    {
+        return File.ReadAllText(fileName);
     }
 }
 
