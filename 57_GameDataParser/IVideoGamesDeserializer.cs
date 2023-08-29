ef9baa577786e96feaeefe59adb859854bd1e75e
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-29 13:25:08 +0300

    29.08. 11

---
+++ 57_GameDataParser/IVideoGamesDeserializer.cs
@@ -0,0 +1,9 @@
+﻿using _57_GameDataParser.Model;
+
+namespace _57_GameDataParser
+{
+    public interface IVideoGamesDeserializer
+    {
+        List<VideoGame> Deserialize(string fileName, string fileContents);
+    }
+}
