b4cc0f54d5f11ea2e3f63322e2dcc880457145de
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-30 19:51:53 +0300

    30.08. 9

--- 57_GameDataParser/DataAccess/IUserInteractor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _57_GameDataParser.DataAccess;
-
-public interface IUserInteractor
-{
-    string ReadValidFilePath();
-
-    void PrintMessage(string message);
-    void PrintError(string message);
-}
