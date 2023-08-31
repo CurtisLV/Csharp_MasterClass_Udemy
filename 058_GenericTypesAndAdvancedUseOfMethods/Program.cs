1e512d75a5b5da6c81b51f1202c97592de744c31
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-31 13:29:21 +0300

    31.08. 4

---
+++ 058_GenericTypesAndAdvancedUseOfMethods/Program.cs
@@ -0,0 +1,5 @@
+﻿var numbers = new List<int> { 1, 3, 1, 2, 41 };
+var words = new List<string> { "aaa", "bbb", "ccc" };
+var dates = new List<DateTime> { new DateTime(2023, 8, 31) };
+
+Console.ReadKey();
