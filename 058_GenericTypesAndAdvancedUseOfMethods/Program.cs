86061e52daecc2549571136d699a14222fe427db
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-08-31 13:52:47 +0300

    31.08. 5

--- 058_GenericTypesAndAdvancedUseOfMethods/Program.cs
+++ 058_GenericTypesAndAdvancedUseOfMethods/Program.cs
@@ -1,5 +1,18 @@
-﻿var numbers = new List<int> { 1, 3, 1, 2, 41 };
-var words = new List<string> { "aaa", "bbb", "ccc" };
-var dates = new List<DateTime> { new DateTime(2023, 8, 31) };
+﻿//var numbers = new List<int> { 1, 3, 1, 2, 41 };
+//var words = new List<string> { "aaa", "bbb", "ccc" };
+//var dates = new List<DateTime> { new DateTime(2023, 8, 31) };
+
+
 
 Console.ReadKey();
+
+class ListOfInts
+{
+    private int[] _items = new int[4];
+    private int _size = 0;
+
+    public void Add(int item)
+    {
+        //
+    }
+}
