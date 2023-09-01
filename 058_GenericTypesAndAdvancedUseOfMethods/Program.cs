b057d1c1a8d8b3da21c6514e9ed7721b14d9dfa5
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-01 13:25:09 +0300

    01.09. 1

--- 058_GenericTypesAndAdvancedUseOfMethods/Program.cs
+++ 058_GenericTypesAndAdvancedUseOfMethods/Program.cs
@@ -14,6 +14,60 @@ numbers.RemoveAt(2);
 
 Console.ReadKey();
 
+class ListOfStrings
+{
+    private int[] _items = new int[4];
+    private int _size = 0;
+
+    public void Add(int item)
+    {
+        if (_size >= _items.Length)
+        {
+            var newItems = new int[_items.Length * 2];
+
+            for (int i = 0; i < _items.Length; i++)
+            {
+                newItems[i] = _items[i];
+            }
+
+            _items = newItems;
+        }
+        _items[_size] = item;
+        ++_size;
+    }
+
+    public void RemoveAt(int index)
+    {
+        // making sure that the index is valid, it should be between zero and the size of the list
+        if (index < 0 || index >= _size)
+        {
+            throw new IndexOutOfRangeException(
+                $"Index {index} is outside of the bounds of the list."
+            );
+        }
+
+        --_size;
+
+        for (int i = index; i < _size; ++i)
+        {
+            _items[i] = _items[i + 1];
+        }
+
+        _items[_size] = 0;
+    }
+
+    public int GetAtIndex(int index)
+    {
+        if (index < 0 || index >= _size)
+        {
+            throw new IndexOutOfRangeException(
+                $"Index {index} is outside of the bounds of the list."
+            );
+        }
+        return _items[index];
+    }
+}
+
 class ListOfInts
 {
     private int[] _items = new int[4];
