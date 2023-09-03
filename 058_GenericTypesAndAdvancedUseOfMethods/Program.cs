576a7729f70bb1f7ad7d725f671f21c42433001d
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-03 11:14:03 +0300

    03.09. 1

--- 058_GenericTypesAndAdvancedUseOfMethods/Program.cs
+++ 058_GenericTypesAndAdvancedUseOfMethods/Program.cs
@@ -59,18 +59,6 @@ Tuple<int, int> GetMinMax(IEnumerable<int> input)
     return new Tuple<int, int>(min, max);
 }
 
-//public class SimpleTuple<T1, T2>
-//{
-//    public SimpleTuple(T1 item1, T2 item2)
-//    {
-//        Item1 = item1;
-//        Item2 = item2;
-//    }
-
-//    public T1 Item1 { get; }
-//    public T2 Item2 { get; }
-//}
-
 class SimpleList<T>
 {
     private T[] _items = new T[4];
