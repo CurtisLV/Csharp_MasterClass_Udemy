5b9b76f8615e5c4e2c2f95c55ee7cd60033e6fff
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-05 18:09:21 +0300

    05.09. 4

--- 058_GenericTypesAndAdvancedUseOfMethods/Program.cs
+++ 058_GenericTypesAndAdvancedUseOfMethods/Program.cs
@@ -131,6 +131,18 @@ public class Pair<T>
     }
 }
 
+public class Point
+{
+    public int X { get; }
+    public int Y { get; }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
 // Second coding exercise
 public static class TupleSwapExercise
 {
