3ea9e78fbf40298cea59e7f452ec01204b1d6660
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-09 12:42:39 +0300

    09.09. 2

--- 058_GenericTypesAndAdvancedUseOfMethods/Program.cs
+++ 058_GenericTypesAndAdvancedUseOfMethods/Program.cs
@@ -93,6 +93,11 @@ var anna = new Person { Name = "Anna", YearOfBirth = 1976 };
 PrintInOrder(10, 5);
 PrintInOrder("aaa", "bbb");
 PrintInOrder(anna, john);
+
+Console.WriteLine($"Square of 2 is {Calculator.Square(2)}");
+Console.WriteLine($"Square of 4m is {Calculator.Square(4m)}");
+Console.WriteLine($"Square of 6d is {Calculator.Square(6d)}");
+
 Console.ReadKey();
 
 void PrintInOrder<T>(T first, T second)
