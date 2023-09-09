75f51b883118d85b6cb34359244fcd316e15b2ee
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-09 12:55:35 +0300

    09.09. 6

---
+++ 058_GenericTypesAndAdvancedUseOfMethods/ListExtensions.cs
@@ -0,0 +1,20 @@
+﻿static class ListExtensions
+{
+    public static List<TTarget> ConvertTo<TSource, TTarget>(this List<TSource> decimals)
+    {
+        var result = new List<TTarget>();
+
+        foreach (var item in decimals)
+        {
+            TTarget itemAfterCasting = (TTarget)Convert.ChangeType(item, typeof(TTarget));
+            result.Add(itemAfterCasting);
+        }
+
+        return result;
+    }
+
+    public static void AddToFront<T>(this List<T> list, T value)
+    {
+        list.Insert(0, value);
+    }
+}
