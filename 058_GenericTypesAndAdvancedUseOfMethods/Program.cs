a60bfb404ce1aa7507e918b7094c74647ff83858
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-17 21:10:10 +0300

    17.09. 1

--- 058_GenericTypesAndAdvancedUseOfMethods/Program.cs
+++ 058_GenericTypesAndAdvancedUseOfMethods/Program.cs
@@ -328,69 +328,6 @@ int SumLengths(string text1, string text2)
 delegate string ProcessString(string input);
 delegate void Print(string input);
 
-public class NumbersFilter
-{
-    public List<int> FilterBy(string filteringType, List<int> numbers)
-    {
-        switch (filteringType)
-        {
-            case "Even":
-                return SelectEven(numbers);
-            case "Odd":
-                return SelectOdd(numbers);
-            case "Positive":
-                return SelectPositive(numbers);
-            default:
-                throw new NotSupportedException($"{filteringType} is not a valid filter");
-        }
-    }
-
-    private List<int> SelectPositive(List<int> nums)
-    {
-        var result = new List<int>();
-
-        foreach (int i in nums)
-        {
-            if (i > 0)
-            {
-                result.Add(i);
-            }
-        }
-
-        return result;
-    }
-
-    private List<int> SelectOdd(List<int> nums)
-    {
-        var result = new List<int>();
-
-        foreach (int i in nums)
-        {
-            if (i % 2 == 1)
-            {
-                result.Add(i);
-            }
-        }
-
-        return result;
-    }
-
-    private List<int> SelectEven(List<int> nums)
-    {
-        var result = new List<int>();
-
-        foreach (int i in nums)
-        {
-            if (i % 2 == 0)
-            {
-                result.Add(i);
-            }
-        }
-
-        return result;
-    }
-}
-
 // Type constraints - numeric types. General math.
 
 public static class Calculator
