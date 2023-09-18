ab9e983e5741a3ee028b35f31640045bef799be7
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-18 18:15:13 +0300

    18.09. 9

--- 058_GenericTypesAndAdvancedUseOfMethods/NumbersFilter.cs
+++ 058_GenericTypesAndAdvancedUseOfMethods/NumbersFilter.cs
@@ -35,17 +35,5 @@ public class FilteringStrategySelector
             throw new NotSupportedException($"{filteringType} is not a valid filter");
         }
         return _filteringStrategies[filteringType];
-
-        switch (filteringType)
-        {
-            case "Even":
-                return number => number % 2 == 0;
-            case "Odd":
-                return number => number % 2 == 1;
-            case "Positive":
-                return number => number > 0;
-            default:
-
-        }
     }
 }
