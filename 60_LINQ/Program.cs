9c96ee67d27a50bcb46e8dffdd6acb0305021252
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-24 13:12:57 +0300

    24.09. 3

--- 60_LINQ/Program.cs
+++ 60_LINQ/Program.cs
@@ -1,4 +1,9 @@
-﻿static bool IsAnyWordUpperCase(IEnumerable<string> words)
+﻿var wordsNoUppercase = new string[] { "apple", "banana", "cherry", "date", "elderberry" };
+Console.WriteLine(IsAnyWordUpperCase(wordsNoUppercase));
+var wordsSomeAllUppercase = new string[] { "Grapes", "kiWi", "Lychee", "ManGo", "ORANGE" };
+Console.WriteLine(IsAnyWordUpperCase(wordsSomeAllUppercase));
+
+static bool IsAnyWordUpperCase(IEnumerable<string> words)
 {
     foreach (var word in words)
     {
