232fecb962fd35f07960d37963d45a0180c1d0af
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-24 13:43:29 +0300

    24.09. 5

--- 60_LINQ/Program.cs
+++ 60_LINQ/Program.cs
@@ -3,6 +3,12 @@ Console.WriteLine(IsAnyWordUpperCase(wordsNoUppercase));
 var wordsSomeAllUppercase = new string[] { "Grapes", "kiWi", "Lychee", "ManGo", "ORANGE" };
 Console.WriteLine(IsAnyWordUpperCase(wordsSomeAllUppercase));
 
+var wordsLetters = new List<string> { "a", "bb", "ccc", "dddd" };
+var wordsLongerThan2Letters = wordsLetters.Where(w => w.Length > 2);
+
+var numbersArray = new int[] { 1, 2, 5, 8, 9, 12, 77, 1, 2 };
+var oddNumbers = numbersArray.Where(num => num % 2 == 1);
+
 static bool IsAnyWordUpperCase(IEnumerable<string> words)
 {
     foreach (var word in words)
