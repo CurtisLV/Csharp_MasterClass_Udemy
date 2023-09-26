abf15cf1e754c6dbe07cc07894328293a156eae3
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-26 16:29:44 +0300

    26.09. 6

--- 60_LINQ/All.cs
+++ 60_LINQ/All.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _60_LINQ.SampleData;
+using _60_LINQ.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,10 @@ static class All
 {
     public static void Run()
     {
-        //
+        var numbers = new[] { 5, 9, 2, 12, 6 };
+        var areAllLargerThanZero = numbers.All(number => number > 0);
+        Printer.Print(areAllLargerThanZero, nameof(areAllLargerThanZero));
+
+        var pets = Data.Pets;
     }
 }
