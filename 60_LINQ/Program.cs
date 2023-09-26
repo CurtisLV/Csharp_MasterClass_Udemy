c3ee75e100a9f74218bd0570f9b3699031388446
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-26 16:26:30 +0300

    26.09. 4

--- 60_LINQ/Program.cs
+++ 60_LINQ/Program.cs
@@ -28,25 +28,10 @@ var animalsWithD = animals.Where(animal =>
     return animal.StartsWith('D');
 });
 
-var numbers = new[] { 5, 9, 2, 12, 6 };
-bool isAnyLargerThan10 = numbers.Any(num => num > 10);
-
 foreach (var animal in animalsWithD)
 {
     Console.WriteLine(animal);
 }
-
-var isAnyPetNamedBruce = Data.Pets.Any(pet => pet.Name == "Bruce");
-Printer.Print(isAnyPetNamedBruce, nameof(isAnyPetNamedBruce));
-
-var isAnyFish = Data.Pets.Any(pet => pet.PetType == PetType.Fish);
-Printer.Print(isAnyFish, nameof(isAnyFish));
-
-var isThereAVerySpecificPet = Data.Pets.Any(pet => pet.Name.Length > 6 && pet.Id % 2 == 0);
-Printer.Print(isThereAVerySpecificPet, nameof(isThereAVerySpecificPet));
-
-var isNotEmpty = Data.Pets.Any();
-Printer.Print(isNotEmpty, nameof(isNotEmpty));
 
 Console.ReadKey();
 
