4b814ce6cc1fb7d9bb34255cba3a4657c37d72d5
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-26 16:27:27 +0300

    26.09. 5

---
+++ 60_LINQ/All.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _60_LINQ;
+
+static class All
+{
+    public static void Run()
+    {
+        //
+    }
+}
