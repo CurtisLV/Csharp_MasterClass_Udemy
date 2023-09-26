a2b5917ca659651b39ccdd6bda7eb188ef987a89
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-26 16:26:05 +0300

    26.09. 3

--- 60_LINQ/Any.cs
+++ 60_LINQ/Any.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _60_LINQ.SampleData;
+using _60_LINQ.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,19 @@ static class Any
 {
     public static void Run()
     {
-        //
+        var numbers = new[] { 5, 9, 2, 12, 6 };
+        bool isAnyLargerThan10 = numbers.Any(num => num > 10);
+
+        var isAnyPetNamedBruce = Data.Pets.Any(pet => pet.Name == "Bruce");
+        Printer.Print(isAnyPetNamedBruce, nameof(isAnyPetNamedBruce));
+
+        var isAnyFish = Data.Pets.Any(pet => pet.PetType == PetType.Fish);
+        Printer.Print(isAnyFish, nameof(isAnyFish));
+
+        var isThereAVerySpecificPet = Data.Pets.Any(pet => pet.Name.Length > 6 && pet.Id % 2 == 0);
+        Printer.Print(isThereAVerySpecificPet, nameof(isThereAVerySpecificPet));
+
+        var isNotEmpty = Data.Pets.Any();
+        Printer.Print(isNotEmpty, nameof(isNotEmpty));
     }
 }
