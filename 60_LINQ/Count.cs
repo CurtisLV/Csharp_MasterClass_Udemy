39ad65736342333347bb48f43ba286400ea46b4e
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-27 11:43:54 +0300

    27.09. 4

--- 60_LINQ/Count.cs
+++ 60_LINQ/Count.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _60_LINQ.SampleData;
+using _60_LINQ.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,9 @@ static class Count
 {
     public static void Run()
     {
-        //
+        var pets = Data.Pets;
+
+        var countOfDogs = pets.Count(pet => pet.PetType == PetType.Dog);
+        Printer.Print(countOfDogs, nameof(countOfDogs));
     }
 }
