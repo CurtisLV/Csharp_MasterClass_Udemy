b14c12eed74686800af3088adb0cba06d1081398
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-28 16:15:26 +0300

    28.09. 3

--- 60_LINQ/Contains.cs
+++ 60_LINQ/Contains.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _60_LINQ.SampleData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace _60_LINQ;
 
 static class Contains
 {
-    //
+    public static void Run()
+    {
+        var pets = Data.Pets;
+    }
 }
