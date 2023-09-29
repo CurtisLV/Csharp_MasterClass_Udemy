77c4c0593ee1bf2228de6019c4a5ab5064f0da63
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-09-29 16:37:06 +0300

    29.09. 3

--- 60_LINQ/OrderBy.cs
+++ 60_LINQ/OrderBy.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _60_LINQ.SampleData;
+using _60_LINQ.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,9 @@ static class OrderBy
 {
     public static void Run()
     {
-        //
+        var pets = Data.Pets;
+
+        var petsOrderedByName = pets.OrderBy(pet => pet.Name);
+        Printer.Print(petsOrderedByName, nameof(petsOrderedByName));
     }
 }
