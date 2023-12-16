c84cd95186c14f932cdf4b8e36b7af34fe483029
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-12-16 20:37:00 +0200

    Im back!

--- 60_LINQ/FirstLast.cs
+++ 60_LINQ/FirstLast.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _60_LINQ.SampleData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace _60_LINQ;
 
-internal class FirstLast
+static class FirstLast
 {
-    //
+    public static void Run()
+    {
+        var pets = Data.Pets;
+    }
 }
