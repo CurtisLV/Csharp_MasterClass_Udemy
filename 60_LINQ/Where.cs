22f4b5e97d1632e85afa094056b5c2f0bf6dcc6f
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-12-17 14:23:37 +0200

    17.12 6

--- 60_LINQ/Where.cs
+++ 60_LINQ/Where.cs
@@ -18,11 +18,20 @@ namespace _60_LINQ
             var evenNumbers = numbers.Where(num => num % 2 == 0);
             Printer.Print(evenNumbers, nameof(evenNumbers));
 
-            var heavierThan10Kilos = 1;
+            var heavierThan10Kilos = pets.Where(pet => pet.Weight > 10);
             Printer.Print(heavierThan10Kilos, nameof(heavierThan10Kilos));
 
-            var heavierThan100Kilos = 1;
+            var heavierThan100Kilos = pets.Where(pet => pet.Weight > 100);
             Printer.Print(heavierThan100Kilos, nameof(heavierThan100Kilos));
+
+            // cats or dogs, name over 4 letters, w 10k and even id
+            var verySpecificPets = pets.Where(
+                    pet => pet.PetType == PetType.Cat || pet.PetType == PetType.Dog
+                )
+                .Where(pet => pet.Weight >= 10)
+                .Where(pet => pet.Id % 2 == 0);
+
+            Printer.Print(verySpecificPets, nameof(verySpecificPets));
         }
     }
 }
