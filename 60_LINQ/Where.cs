77f0ecd2525217827b9c501c86091e2c63cb8f9e
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-12-17 14:34:27 +0200

    17.12 10

--- 60_LINQ/Where.cs
+++ 60_LINQ/Where.cs
@@ -1,10 +1,5 @@
 ﻿using _60_LINQ.SampleData;
 using _60_LINQ.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _60_LINQ
 {
