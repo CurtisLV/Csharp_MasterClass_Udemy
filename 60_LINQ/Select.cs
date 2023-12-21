830d21b43f1c5195fdf3073eb33c9ca05244ac1e
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-12-21 11:43:58 +0200

    21.12 5

---
+++ 60_LINQ/Select.cs
@@ -0,0 +1,18 @@
+﻿using _60_LINQ.SampleData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _60_LINQ
+{
+    static class Select
+    {
+        public static void Run()
+        {
+            var pets = Data.Pets;
+            var numbers = Data.numbers;
+        }
+    }
+}
