0907445d99e9c45b6833dc66b55f67a1e686302e
Mukāns, Kārlis <karlis.mukans@seb.se>
2023-12-23 13:58:01 +0200

    23.12. 2

--- 60_LINQ/Average.cs
+++ 60_LINQ/Average.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _60_LINQ.SampleData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace _60_LINQ
 {
-    internal class Average
+    static class Average
     {
+        public static void Run()
+        {
+            var pets = Data.Pets;
+            var numbers = Data.numbers;
+        }
     }
 }
