3cf087be4a96f80ffd442aeca00ffc3e1c269b0f
Kārlis Mukāns <karlis.mukans@seb.se>
2024-05-31 10:22:53 +0300

    31.05 7

--- CookieCookbook/Recipes/Ingredients/IngredientsRegister.cs
+++ CookieCookbook/Recipes/Ingredients/IngredientsRegister.cs
@@ -17,14 +17,6 @@ public class IngredientsRegister : IIngredientsRegister
 
     public Ingredient GetById(int id)
     {
-        foreach (var ingredient in All)
-        {
-            if (ingredient.Id == id)
-            {
-                return ingredient;
-            }
-        }
-
-        return null;
+        return All.FirstOrDefault(ingredient => ingredient.Id == id);
     }
 }
