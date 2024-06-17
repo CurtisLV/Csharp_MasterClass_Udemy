ee247f1c730b36b71095dae1590fb49db258341e
Kārlis Mukāns <karlis.mukans@seb.se>
2024-06-17 18:48:11 +0300

    17.06. 5

--- 61_dotnet_underHood/Program.cs
+++ 61_dotnet_underHood/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int number = 5;
+int anothNum = number;
+
+anothNum++;
+
+Console.WriteLine("Number is " + number);
+Console.WriteLine("2nd number is " + anothNum);
