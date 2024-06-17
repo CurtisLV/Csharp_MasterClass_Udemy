382e12f20abffe33b9dec8af68ed4fbad943771a
Kārlis Mukāns <karlis.mukans@seb.se>
2024-06-17 18:02:18 +0300

    17.06. 1

--- CookieCookbook/Recipes/Ingredients/IngredientsRegister.cs
+++ CookieCookbook/Recipes/Ingredients/IngredientsRegister.cs
@@ -18,12 +18,19 @@ public class IngredientsRegister : IIngredientsRegister
     public Ingredient GetById(int id)
     {
         var allIngredientsWithGivenId = All.Where(ingredient => ingredient.Id == id);
+
         if (allIngredientsWithGivenId.Count() > 1)
         {
             throw new InvalidOperationException(
                 $"More than one ingredients have given ID equal {id}."
             );
         }
+
+        if (All.Select(ingredient => ingredient.Id).Distinct().Count() != All.Count())
+        {
+            throw new InvalidOperationException($"Some ingredients have duplicated IDs.");
+        }
+
         return allIngredientsWithGivenId.FirstOrDefault();
     }
 }
