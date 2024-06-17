479fc486429fad6ec2610b6bc8c83f4c7175d5e7
Kārlis Mukāns <karlis.mukans@seb.se>
2024-06-17 18:51:32 +0300

    17.06. 6

--- 61_dotnet_underHood/Program.cs
+++ 61_dotnet_underHood/Program.cs
@@ -7,3 +7,17 @@ anothNum++;
 
 Console.WriteLine("Number is " + number);
 Console.WriteLine("2nd number is " + anothNum);
+
+var john = new Person { Name = "John", Age = 35 };
+var person = john;
+
+person.Age = 44;
+
+Console.WriteLine("Johns age " + john.Age);
+Console.WriteLine("Persons age " + person.Age);
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
