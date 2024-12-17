c1cb5238f8008e230fdd00c112b041f39c2db775
Kārlis Mukāns <karlis.mukans@seb.se>
2024-12-17 15:38:46 +0200

    17.12. 5

--- 61_dotnet_underHood/FileWriter.cs
+++ 61_dotnet_underHood/FileWriter.cs
@@ -18,5 +18,26 @@ public class FileWriter
     public void Write(string text)
     {
         _streamWriter.WriteLine(text);
+        _streamWriter?.Flush();
+    }
+}
+
+public class SpecificLineFromTextFileReader
+{
+    private readonly StreamReader _streamReader;
+
+    public SpecificLineFromTextFileReader(string filepath)
+    {
+        _streamReader = new StreamReader(filepath);
+    }
+
+    public string ReadLineNumber(int lineNumber)
+    {
+        for (int i = 0; i < lineNumber - 1; i++)
+        {
+            _streamReader.ReadLine();
+        }
+
+        return _streamReader.ReadLine();
     }
 }
