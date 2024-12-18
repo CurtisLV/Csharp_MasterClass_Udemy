246519e08d6fe8b831dd83c54c25d1f1f8e41d96
Kārlis Mukāns <karlis.mukans@seb.se>
2024-12-18 10:43:31 +0200

    18.12. 1

--- 61_dotnet_underHood/Program.cs
+++ 61_dotnet_underHood/Program.cs
@@ -162,6 +162,6 @@ public class AllLinesFromTextFileReader : IDisposable
 
     public void Dispose()
     {
-        throw new NotImplementedException();
+        _streamReader.Dispose();
     }
 }
