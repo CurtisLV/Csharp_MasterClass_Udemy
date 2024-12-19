baff7dd863e369e264ff0c8cc2369f51ee57fe3c
Kārlis Mukāns <karlis.mukans@seb.se>
2024-12-19 13:57:57 +0200

    19.12. 3

--- 07_NET_UnderTheHood_AssignmentStartupProject/NewSolution/FastTableDataBuilder.cs
+++ 07_NET_UnderTheHood_AssignmentStartupProject/NewSolution/FastTableDataBuilder.cs
@@ -56,3 +56,18 @@ public class FastTableDataBuilder : ITableDataBuilder
         return value;
     }
 }
+
+public class FastRow
+{
+    private Dictionary<string, object> _data;
+
+    public FastRow(Dictionary<string, object> data)
+    {
+        _data = data;
+    }
+
+    public object GetAtColumn(string columnName)
+    {
+        return _data[columnName];
+    }
+}
