f73fc3e2a85ac5a65ddf9f7d6b3a6fd465697e5b
Kārlis Mukāns <karlis.mukans@seb.se>
2024-12-20 14:28:35 +0200

    20.12. 1

---
+++ 62_AdvancedCsharpTypes/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Person
+{
+    public string Name { get; }
+    public int YearOfBirth { get; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        YearOfBirth = age;
+    }
+
+    public Person(string name) => Name = name;
+}
