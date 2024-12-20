527893dcaefbecd540e51045ef2e009eb22e59d7
Kārlis Mukāns <karlis.mukans@seb.se>
2024-12-20 14:44:51 +0200

    20.12. 4

--- 62_AdvancedCsharpTypes/Program.cs
+++ 62_AdvancedCsharpTypes/Program.cs
@@ -1,15 +1,27 @@
-﻿Console.ReadKey();
+﻿using System.ComponentModel.DataAnnotations;
+
+var validPerson = new Person("John", 1989);
+var invalidDog = new Dog("F");
+
+var validator = new Validator();
+
+Console.WriteLine(validator.Validate(validPerson) ? "Person is valid" : "Person is invalid");
+Console.WriteLine(validator.Validate(invalidDog) ? "Dog is valid" : "Dog is invalid");
+
+Console.ReadKey();
 
 public class Dog
 {
-    public string Name { get; } // length must be between 2 and 10 chars
+    [StringLengthValidate(2, 10)]
+    public string Name { get; } // length must be between 2 and 10
 
     public Dog(string name) => Name = name;
 }
 
 public class Person
 {
-    public string Name { get; }
+    [StringLengthValidate(2, 25)]
+    public string Name { get; } // length must be between 2 and 25
     public int YearOfBirth { get; }
 
     public Person(string name, int age)
