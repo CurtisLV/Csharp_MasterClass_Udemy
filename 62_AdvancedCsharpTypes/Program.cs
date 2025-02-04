19099d0d7d25a782548ff9d3274b43ba73a00da2
Kārlis Mukāns <karlis.mukans@seb.se>
2025-02-04 14:10:23 +0200

    04.02. 7

--- 62_AdvancedCsharpTypes/Program.cs
+++ 62_AdvancedCsharpTypes/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var validPerson = new PersonToBeValidated("John", 1989);
 var invalidDog = new Dog("F");
@@ -254,6 +256,8 @@ var requestUri = "data?drilldowns=Nation&measures=Population";
 IApiDataReader apiDataReader = new ApiDataReader();
 var json = await apiDataReader.Read(baseAddress, requestUri);
 
+var root = JsonSerializer.Deserialize<Root>(json);
+
 Console.ReadKey();
 
 public interface IApiDataReader
@@ -275,6 +279,38 @@ public class ApiDataReader() : IApiDataReader
         return await response.Content.ReadAsStringAsync();
     }
 }
+
+// Root myDeserializedClass = JsonSerializer.Deserialize<Root>(myJsonResponse);
+public record Annotations(
+    [property: JsonPropertyName("source_name")] string source_name,
+    [property: JsonPropertyName("source_description")] string source_description,
+    [property: JsonPropertyName("dataset_name")] string dataset_name,
+    [property: JsonPropertyName("dataset_link")] string dataset_link,
+    [property: JsonPropertyName("table_id")] string table_id,
+    [property: JsonPropertyName("topic")] string topic,
+    [property: JsonPropertyName("subtopic")] string subtopic
+);
+
+public record Datum(
+    [property: JsonPropertyName("ID Nation")] string IDNation,
+    [property: JsonPropertyName("Nation")] string Nation,
+    [property: JsonPropertyName("ID Year")] int IDYear,
+    [property: JsonPropertyName("Year")] string Year,
+    [property: JsonPropertyName("Population")] int Population,
+    [property: JsonPropertyName("Slug Nation")] string SlugNation
+);
+
+public record Root(
+    [property: JsonPropertyName("data")] IReadOnlyList<Datum> data,
+    [property: JsonPropertyName("source")] IReadOnlyList<Source> source
+);
+
+public record Source(
+    [property: JsonPropertyName("measures")] IReadOnlyList<string> measures,
+    [property: JsonPropertyName("annotations")] Annotations annotations,
+    [property: JsonPropertyName("name")] string name,
+    [property: JsonPropertyName("substitutions")] IReadOnlyList<object> substitutions
+);
 
 // Attributes
 
