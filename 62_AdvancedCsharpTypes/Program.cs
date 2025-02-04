cf66093e488119e7c7f8ac1dd1740f50f3034ea6
Kārlis Mukāns <karlis.mukans@seb.se>
2025-02-04 11:55:06 +0200

    04.02. 3

--- 62_AdvancedCsharpTypes/Program.cs
+++ 62_AdvancedCsharpTypes/Program.cs
@@ -252,9 +252,11 @@ using var client = new HttpClient();
 
 //client.BaseAddress = new Uri("https://datausa.io/api/data?drilldowns=Nation&measures=Population");
 client.BaseAddress = new Uri("https://datausa.io/api/");
-var response = await client.GetAsync("data?drilldowns=Nation&measures=Population");
+HttpResponseMessage response = await client.GetAsync("data?drilldowns=Nation&measures=Population");
 
 response.EnsureSuccessStatusCode();
+
+string json = await response.Content.ReadAsStringAsync();
 
 Console.ReadKey();
 
