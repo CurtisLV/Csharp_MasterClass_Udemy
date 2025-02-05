877eedf912ca2dcf272e50a59db28f93844a31c6
Kārlis Mukāns <karlis.mukans@seb.se>
2025-02-05 19:26:03 +0200

    05.02. 5

--- 62.1_StarWarsAPI_Assignment/DTOs/Root.cs
+++ 62.1_StarWarsAPI_Assignment/DTOs/Root.cs
@@ -1,6 +1,10 @@
-﻿namespace _62._1_StarWarsAPI_Assignment.DTOs;
+﻿using System.Text.Json.Serialization;
 
-public class Root
-{
-    //
-}
+namespace _62._1_StarWarsAPI_Assignment.DTOs;
+
+public record Root(
+    [property: JsonPropertyName("count")] int count,
+    [property: JsonPropertyName("next")] string next,
+    [property: JsonPropertyName("previous")] object previous,
+    [property: JsonPropertyName("results")] IReadOnlyList<Result> results
+);
