5464f46d7dabab90dec8dcf4446daa1a09a80cb9
Kārlis Mukāns <karlis.mukans@seb.se>
2025-02-05 19:04:00 +0200

    05.02. 4

--- 62.1_StarWarsAPI_Assignment/DTOs/Root.cs
+++ 62.1_StarWarsAPI_Assignment/DTOs/Root.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace _62._1_StarWarsAPI_Assignment.DTOs;
 
-namespace _62._1_StarWarsAPI_Assignment.DTOs
+public class Root
 {
-    internal class Root
-    {
-        //
-    }
+    //
 }
