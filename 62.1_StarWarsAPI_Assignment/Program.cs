fb9da8742fc5c61f568072a456746fb6e9f4bc16
Kārlis Mukāns <karlis.mukans@seb.se>
2025-02-05 19:03:16 +0200

    05.02.

--- 62.1_StarWarsAPI_Assignment/Program.cs
+++ 62.1_StarWarsAPI_Assignment/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using System.Text.Json;
+
+var baseAddress = "https://datausa.io";
+var requestUri = " /api/planets/";
+
+IApiDataReader apiDataReader = new ApiDataReader();
+var json = await apiDataReader.Read(baseAddress, requestUri);
+
+var root = JsonSerializer.Deserialize<Root>(json);
