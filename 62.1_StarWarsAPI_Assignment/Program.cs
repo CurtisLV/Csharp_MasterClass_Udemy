a08ac8b2e7df612b588d6d11c028d3e27a6daca6
Kārlis Mukāns <karlis.mukans@seb.se>
2025-02-06 18:25:53 +0200

    06.02. 2

--- 62.1_StarWarsAPI_Assignment/Program.cs
+++ 62.1_StarWarsAPI_Assignment/Program.cs
@@ -11,7 +11,31 @@ var root = JsonSerializer.Deserialize<Root>(json);
 
 foreach (var planet in root.results)
 {
-    Console.WriteLine($"Planet: {planet.name}, {}");
+    Console.WriteLine(
+        $"Planet: {planet.name}, Diameter: {planet.diameter}, Surfacewater: {planet.surface_water}, Population: {planet.population}"
+    );
+}
+
+// Define the column widths
+const int nameWidth = 20;
+const int diameterWidth = 10;
+const int surfaceWaterWidth = 15;
+const int populationWidth = 15;
+
+// Print the header
+Console.WriteLine(
+    $"{"Planet", -nameWidth} | {"Diameter", -diameterWidth} | {"Surfacewater", -surfaceWaterWidth} | {"Population", -populationWidth}"
+);
+Console.WriteLine(
+    new string('-', nameWidth + diameterWidth + surfaceWaterWidth + populationWidth + 9)
+);
+
+// Print the data
+foreach (var planet in root.results)
+{
+    Console.WriteLine(
+        $"{planet.name, -nameWidth} | {planet.diameter, -diameterWidth} | {planet.surface_water, -surfaceWaterWidth} | {planet.population, -populationWidth}|"
+    );
 }
 
 Console.ReadKey();
